912092f543e0f37e2db395504a611013b6ff27e8
lameR <erpresents@gmail.com>
2021-03-17 17:45:31 +0300

    SUP-15015 Создать ШП с QR кодом

--- b15_s12/SUP-15015.sql
+++ b15_s12/SUP-15015.sql
@@ -4,4 +4,4 @@ select * from rbPrintTemplate rpt where id = 389
 select * from Person p where p.userProfile_id = 1;
 
 
-select * from rbPrintTemplate rpt where `default` REGEXP ''
+select * from rbPrintTemplate rpt where `default` REGEXP 'base64'
