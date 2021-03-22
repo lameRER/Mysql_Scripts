f80c66215a6e0e8c1a261806f130a1ac2fbbc400
lameR <erpresents@gmail.com>
2021-03-22 12:43:52 +0300

    SUP-15535

--- sochi4/SUP-15538.sql
+++ sochi4/SUP-15538.sql
@@ -1,16 +1,21 @@
-select * from logger.login l order by id desc ;
+select * from logger.login where revision is not null order by id desc ;
 
 
-select * from ActionPropertyType a 
-where a.typeName = 'Person' and valueDomain != ''-- and a.name regexp '������';
+select * from ActionPropertyType a
+where a.typeName = 'Person' and valueDomain != ''-- and a.name regexp 'пїЅпїЅпїЅпїЅпїЅпїЅ';
 
 
 select o.name, p.login, p.password from person p 
-left join orgstructure o on o.id = p.orgStructure_id 
+left join orgstructure o on o.id = p.orgStructure_id
 where p.userProfile_id = 1;
 
 
 select * from person p where p.deleted = 0 and p.retireDate is null and p.speciality_id is not null;
 
 
-select p.login, p.password from person p where p.password = md5('654321');
+select p.id, p.login, p.userProfile_id, p.password from person p where p.password = md5('654321');
+
+
+update person p, person_userprofile pup
+set p.userProfile_id = 1, pup.userProfile_id = 1
+where p.id = 303 and p.id = pup.person_id 
