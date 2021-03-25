4280a7cee2769bcba56ce1effaa0e94b44b94aec
lameR <erpresents@gmail.com>
2021-03-25 14:25:31 +0300

    mv

---
+++ b15/SUP-15619.sql
@@ -0,0 +1,237 @@
+select *
+from Event e
+where e.client_id = 846712;
+select count(rep.id) num,
+    -- GROUP_CONCAT(CONCAT_WS(' ',ret.code, ret.name,'retres.idx:',retres.idx,'resper.idx:',resrep.idx) separator '\n') ret,
+    -- GROUP_CONCAT(CONCAT_WS(' ', retres.id, resrep.id, res.id, res.name, res.description)separator '\n') res,
+    ret.*
+from rbEpicrisisTemplates ret
+    left join rbEpicrisisTemplates_rbEpicrisisSections retres on retres.id_rbEpicrisisTemplates = ret.id
+    left join rbEpicrisisSections res on retres.id_rbEpicrisisSections = res.id
+    left join rbEpicrisisSections_rbEpicrisisProperty resrep on resrep.id_rbEpicrisisSections = res.id
+    left join rbEpicrisisProperty rep on resrep.id_rbEpicrisisProperty = rep.id
+where ret.code = 'Код 2' -- where rep.name REGEXP 'Анамнез заболевания'
+group by res.id,
+    rep.id
+order by retres.idx,
+    resrep.idx;
+-- order by rep.id
+-- insert into rbEpicrisisTemplates (name, code, id_orgStructure)
+-- select ret.name, 'Код 2' code, ret.id_orgStructure from rbEpicrisisTemplates ret where ret.name = 'Выписной эпикриз';
+select *
+from rbEpicrisisTemplates ret;
+select *
+from rbEpicrisisProperty rep;
+select *
+from rbEpicrisisSections res;
+INSERT into rbEpicrisisSections (name, description)
+select 'Отделение' name,
+    'Шапка эпикриза' description;
+INSERT into rbEpicrisisProperty (name, description, `type`, defaultValue)
+select 'Отделение' name,
+    'Шапка эпикриза' description,
+    8 `type`,
+    'select os.name from OrgStructure os\nwhere os.id in(12,13,14,15,16,17,18,19,20,22,23,24) ORDER by os.name' defaultValue
+INSERT into rbEpicrisisSections_rbEpicrisisProperty (
+        id_rbEpicrisisSections,
+        id_rbEpicrisisProperty,
+        idx,
+        htmlTemplate,
+        orgStruct,
+        isRequired,
+        isEditable
+    )
+select res.id id_rbEpicrisisSections,
+    rep.id id_rbEpicrisisProperty,
+    0 idx,
+    resrep.htmlTemplate,
+    resrep.orgStruct,
+    resrep.isRequired,
+    resrep.isEditable
+from rbEpicrisisSections_rbEpicrisisProperty resrep,
+    rbEpicrisisProperty rep,
+    rbEpicrisisSections res
+where rep.name = 'Отделение'
+    and res.name = 'Отделение'
+GROUP by res.id
+INSERT into rbEpicrisisTemplates_rbEpicrisisSections (
+        id_rbEpicrisisTemplates,
+        id_rbEpicrisisSections,
+        idx,
+        isEditable,
+        htmlTemplate,
+        isRequired
+    )
+select 4 id_rbEpicrisisTemplates,
+    res.id id_rbEpicrisisSections,
+    0 idx,
+    isEditable,
+    htmlTemplate,
+    isRequired
+FROM rbEpicrisisTemplates_rbEpicrisisSections,
+    rbEpicrisisSections res
+where res.name = 'Отделение'
+GROUP by res.id
+select '' `Отделение`
+UNION
+(
+    select os.name `Отделение`
+    from OrgStructure os
+    where os.id in(12, 13, 14, 15, 16, 17, 18, 19, 20, 22, 23, 24)
+)
+ORDER by `Отделение` -- UPDATE
+select *
+from rbEpicrisisProperty rep -- set rep.`type` = 9, defaultValue = 'select ''\nunion\nselect os.name from OrgStructure os\nwhere os.id in(12,13,14,15,16,17,18,19,20,22,23,24) ORDER by os.name'
+where rep.name = 'отделение';
+select os.name
+from OrgStructure os
+where os.id in(12, 13, 14, 15, 16, 17, 18, 19, 20, 22, 23, 24)
+ORDER by name
+set @return2 = null;
+set @return3 = null;
+set @return2 = (
+        SELECT GROUP_CONCAT(ep.id)
+        FROM rbEpicrisisProperty ep
+        WHERE (
+                (
+                    ep.name REGEXP 'ФИО пациента'
+                    /*and description = 'Проведенное лечение'*/
+                ) -- or (ep.name REGEXP 'Заключение' and description = 'Предоперационный эпикриз КХО')
+            )
+    );
+SET @return = (
+        SELECT GROUP_CONCAT(et.id)
+        FROM rbEpicrisisTemplates et
+        WHERE et.code = '14'
+    );
+set @return3 = (
+        SELECT GROUP_CONCAT(es.id)
+        FROM rbEpicrisisSections es
+        WHERE (
+                es.name REGEXP ''
+                /*and description = ''*/
+                /*AND es.description = (SELECT et.name FROM rbEpicrisisTemplates et WHERE et.id= @return)*/
+            )
+    );
+SET @es_ai_s := @es_ai_e := (
+        SELECT `AUTO_INCREMENT`
+        FROM INFORMATION_SCHEMA.TABLES
+        WHERE TABLE_SCHEMA = 'pes'
+            AND TABLE_NAME = 'rbEpicrisisSections'
+    );
+INSERT LOW_PRIORITY rbEpicrisisSections(id, name, description)
+SELECT *
+FROM (
+        SELECT @es_ai_e := @es_ai_e + 1,
+            '' name,
+            (
+                SELECT et.name
+                FROM rbEpicrisisTemplates et
+                WHERE FIND_IN_SET(et.id, @return)
+                limit 1
+            ) description
+    ) tmp
+WHERE NOT EXISTS(
+        SELECT *
+        FROM rbEpicrisisSections es
+        WHERE es.name = tmp.name
+            AND es.description = tmp.description
+    );
+set @return3 = NULL;
+INSERT LOW_PRIORITY rbEpicrisisSections_rbEpicrisisProperty(
+        id_rbEpicrisisSections,
+        id_rbEpicrisisProperty,
+        idx
+    )
+SELECT es.id,
+    ep.id,
+    ROW_NUMBER() OVER(
+        ORDER BY esep.idx
+    ) + IFNULL(
+        (
+            SELECT MAX(esep.idx)
+            FROM rbEpicrisisSections_rbEpicrisisProperty esep
+            WHERE esep.id_rbEpicrisisSections = (
+                    SELECT MAX(es1.id)
+                    FROM rbEpicrisisSections es1
+                )
+        ),
+        0
+    )
+FROM rbEpicrisisProperty ep,
+    rbEpicrisisSections es,
+    rbEpicrisisSections_rbEpicrisisProperty esep
+WHERE (
+        (
+            es.id BETWEEN @es_ai_s AND @es_ai_e
+        )
+        or es.id = @return3
+    )
+    AND (
+        (
+            ep.id BETWEEN @ep_ai_s AND @ep_ai_e
+        )
+        or(FIND_IN_SET(ep.id, @return2)) = IF(
+            esep.id_rbEpicrisisProperty = ep.id,
+            esep.id_rbEpicrisisProperty = ep.id,
+            ep.id
+        )
+    )
+GROUP BY ep.id;
+set @idx_count = 0;
+INSERT LOW_PRIORITY rbEpicrisisTemplates_rbEpicrisisSections(
+        id_rbEpicrisisTemplates,
+        id_rbEpicrisisSections,
+        idx
+    )
+SELECT *
+FROM (
+        SELECT et.id et_id,
+            es.id es_id,
+            @idx_count := @idx_count + 1 + IFNULL(
+                (
+                    SELECT MAX(etes.idx)
+                    FROM rbEpicrisisTemplates_rbEpicrisisSections etes
+                    WHERE etes.id_rbEpicrisisTemplates = FIND_IN_SET(et.id, @return)
+                ),
+                0
+            )
+        FROM rbEpicrisisTemplates et,
+            rbEpicrisisSections es,
+            rbEpicrisisTemplates_rbEpicrisisSections etes
+        WHERE (
+                (
+                    es.id BETWEEN @es_ai_s AND @es_ai_e
+                )
+                OR FIND_IN_SET(es.id, @return3)
+            )
+            AND et.id = @return
+        GROUP BY es.id
+    ) tmp
+WHERE NOT EXISTS(
+        SELECT *
+        FROM rbEpicrisisTemplates_rbEpicrisisSections etes
+        WHERE tmp.es_id = etes.id_rbEpicrisisSections
+            AND tmp.et_id = etes.id_rbEpicrisisTemplates
+    );
+-- INSERT LOW_PRIORITY rbEpicrisisTemplates_rbEpicrisisSections(id_rbEpicrisisTemplates, id_rbEpicrisisSections, idx)
+-- SELECT * FROM (SELECT et.id et_id, es.id es_id, IFNULL((SELECT MAX(etes.idx) FROM rbEpicrisisTemplates_rbEpicrisisSections etes WHERE FIND_IN_SET(etes.id_rbEpicrisisTemplates ,@return)),0) FROM rbEpicrisisTemplates et, rbEpicrisisSections es, rbEpicrisisTemplates_rbEpicrisisSections etes
+-- WHERE ((es.id BETWEEN @es_ai_s AND @es_ai_e) OR FIND_IN_SET(es.id,@return3)) AND FIND_IN_SET(et.id, @return) GROUP BY es.id,et.id) tmp
+-- WHERE NOT EXISTS(SELECT * FROM rbEpicrisisTemplates_rbEpicrisisSections etes WHERE tmp.es_id = etes.id_rbEpicrisisSections AND tmp.et_id = etes.id_rbEpicrisisTemplates);
+select *
+from rbEpicrisisTemplates ret
+    left join rbEpicrisisTemplates_rbEpicrisisSections retres on retres.id_rbEpicrisisTemplates = ret.id
+    left join rbEpicrisisSections res on retres.id_rbEpicrisisSections = res.id
+    left join rbEpicrisisSections_rbEpicrisisProperty resrep on resrep.id_rbEpicrisisSections = res.id
+    left join rbEpicrisisProperty rep on resrep.id_rbEpicrisisProperty = rep.id
+where FIND_IN_SET(ret.id, @return)
+    AND (
+        rep.id BETWEEN @ep_ai_s and @ep_ai_e
+    )
+    AND (
+        res.id BETWEEN @es_ai_s and @es_ai_e
+    )
+order by retres.idx,
+    resrep.idx;
+SET @ep_ai_s := @ep_ai_e := @es_ai_s := @es_ai_e := NULL;
+-- commit;
---
+++ b15/SUP-15696.sql
@@ -0,0 +1,218 @@
+select count(rep.id) num,
+-- GROUP_CONCAT(CONCAT_WS(' ', ret.id_orgStructure , ret.code, ret.name,'retres.idx:',retres.idx,'resper.idx:',resrep.idx) separator '\n') ret, 
+-- GROUP_CONCAT(CONCAT_WS(' ', retres.id, resrep.id, res.id, res.name, res.description)separator '\n') res, 
+-- retres.* 
+resrep.*
+from rbEpicrisisTemplates ret
+left join rbEpicrisisTemplates_rbEpicrisisSections retres on retres.id_rbEpicrisisTemplates = ret.id 
+left join rbEpicrisisSections res on retres.id_rbEpicrisisSections = res.id 
+left join rbEpicrisisSections_rbEpicrisisProperty resrep on resrep.id_rbEpicrisisSections = res.id 
+left join rbEpicrisisProperty rep on resrep.id_rbEpicrisisProperty = rep.id
+where ret.code = 'код 2'
+group by res.id, rep.id 
+order by  retres.idx, resrep.idx;
+
+
+set @ret = 1;
+INSERT into rbEpicrisisTemplates_rbEpicrisisSections (id_rbEpicrisisTemplates, id_rbEpicrisisSections, idx)
+select ret.id id_rbEpicrisisTemplates, res.id id_rbEpicrisisSections, @ret:=@ret+1 idx from rbEpicrisisTemplates ret, rbEpicrisisSections res
+where ret.code = 'код 2'
+
+
+select * from rbEpicrisisSections res ;
+
+
+select rep.* from rbEpicrisisProperty rep, rbEpicrisisSections_rbEpicrisisProperty resrep
+where id_rbEpicrisisSections = 1 and resrep.id_rbEpicrisisProperty = rep.id 
+ORDER by resrep.idx;
+
+
+INSERT into rbEpicrisisProperty (name, description, `type`, defaultValue, valueDomain, printAsTable, isCopy)
+select 'Адрес проживания' name, rep.description, rep.`type`, rep.defaultValue, rep.valueDomain, rep.printAsTable, rep.isCopy from rbEpicrisisProperty rep where rep.id = 5
+
+INSERT into rbEpicrisisSections_rbEpicrisisProperty (id_rbEpicrisisSections, id_rbEpicrisisProperty, idx)
+select 1 id_rbEpicrisisSections, rep.id id_rbEpicrisisProperty, (select max(resrep.idx) from rbEpicrisisSections_rbEpicrisisProperty resrep where resrep.id_rbEpicrisisSections = (select res.id from rbEpicrisisSections res where res.id = 1))+1 idx from rbEpicrisisProperty rep where rep.name = 'Адрес проживания';
+
+
+select * from rbEpicrisisSections_rbEpicrisisProperty resrep where resrep.id_rbEpicrisisSections = 1 order by resrep.idx;
+
+
+select * from rbEpicrisisTemplates_rbEpicrisisSections retres 
+where retres.id_rbEpicrisisTemplates = 4
+
+
+
+select * from rbEpicrisisSections_rbEpicrisisProperty resrep where resrep.id_rbEpicrisisSections = 1 ORDER by resrep.idx ;
+
+select * from rbEpicrisisSections_rbEpicrisisProperty resrep where resrep.id_rbEpicrisisSections = 11 ORDER by resrep.idx ;
+
+select * from rbEpicrisisSections res where id = 11;
+
+
+select * from rbEpicrisisTemplates_rbEpicrisisSections  where id_rbEpicrisisTemplates  = 4 ORDER by idx ;
+
+
+
+
+select * from rbEpicrisisTemplates_rbEpicrisisSections retres 
+where retres.id_rbEpicrisisTemplates = (select ret.id from rbEpicrisisTemplates ret where ret.code = 'Код 2')
+
+select ret.* from rbEpicrisisTemplates ret where ret.code = 'Код 2'
+
+
+select * from rbEpicrisisProperty rep order by id desc;
+
+
+ 
+
+select apt.* from ActionPropertyType apt 
+join ActionType at2 on apt.actionType_id = at2.id and at2.deleted = 0 and at2.code = '1-1-2' 
+where apt.deleted = 0 ORDER by apt.idx
+
+
+select apt.name `Название поля`, aps.value `Значение поля` from ActionProperty ap
+left join ActionProperty_String aps using(id)
+join Action a on ap.action_id = a.id and a.deleted = 0 and a.status = 2 and a.event_id = %s
+join ActionType at2 on at2.id = a.actionType_id and at2.deleted = 0 and at2.code = '1-1-2'
+join ActionPropertyType apt on apt.actionType_id = at2.id and ap.type_id = apt.id and apt.name REGEXP '
+Сознание|Температура|Кожные покровы|Видимые слизистые|Периферические лимфоузлы|Пульс|Сатурация|Артериальное давление|Тоны сердце|Шумы сердца|Част. дых|Дыхание|Хрипы|Язык|Живот|При пальпации|напряжен в|Симптомы раздражения брюшины|Печень|Поколачивание в поясничной области|Костно-мышечная система, суставы|Физиологические оправления' and apt.deleted = 0
+where ap.deleted = 0 ORDER by a.createDatetime desc, apt.idx 
+
+
+select rep.*
+from rbEpicrisisSections_rbEpicrisisProperty resrep, rbEpicrisisSections res, rbEpicrisisProperty rep 
+where resrep.id_rbEpicrisisSections = res.id and res.id = 5 and resrep.id_rbEpicrisisProperty = rep.id
+ GROUP by resrep.id ORDER by resrep.idx
+ 
+ 
+select * from rbEpicrisisSections_rbEpicrisisProperty resrep where resrep.id_rbEpicrisisSections = 5;
+
+
+ select p.org_id from Person p ;	
+ 
+ 
+ select * from rbDiagnosisType rdt1 ;
+ 
+
+INSERT into rbEpicrisisSections (name , description)
+select 'Результаты исследований' name, 'Результаты исследований' description
+
+
+select * from rbEpicrisisSections res order by id desc;
+
+
+INSERT into rbEpicrisisTemplates_rbEpicrisisSections (id_rbEpicrisisTemplates, id_rbEpicrisisSections, idx, isEditable, htmlTemplate, isRequired)
+select ret.id id_rbEpicrisisTemplates, 12 id_rbEpicrisisSections, max(retres.idx)+1 idx, retres.isEditable, retres.htmlTemplate, retres.isRequired from rbEpicrisisTemplates_rbEpicrisisSections retres, rbEpicrisisTemplates ret 
+where retres.id_rbEpicrisisTemplates = ret.id and ret.code = 'Код 2' 
+
+set @ret = 0;
+INSERT into rbEpicrisisSections_rbEpicrisisProperty (id_rbEpicrisisSections, id_rbEpicrisisProperty, idx, htmlTemplate, orgStruct, isRequired, isEditable)
+select 12 id_rbEpicrisisSections, rep.id id_rbEpicrisisProperty, @ret:=@ret+1 idx, resrep.htmlTemplate, resrep.orgStruct, resrep.isRequired, resrep.isEditable from rbEpicrisisSections_rbEpicrisisProperty resrep, rbEpicrisisProperty rep 
+where rep.id in(75,74) 
+GROUP  by rep.id
+
+
+INSERT INTO rbEpicrisisProperty (name, description, `type`)
+select 'Лабораторные исследования:' name, 'Результаты исследований' description, 8 `type`
+union 
+select 'Инструментальные методы:' name, 'Результаты исследований' description, 8 `type`
+
+
+select * from rbEpicrisisProperty rep order by rep.id desc;
+
+
+
+select at2.name, a.* from Action a
+join ActionType at2 on at2.id = a.actionType_id and at2.deleted = 0
+where a.deleted = 0 and a.event_id = 33721812
+order by a.createDatetime desc 
+
+
+
+
+select * from ActionType at2
+where at2.group_id = (select at3.id from ActionType at3 where at3.code = 'analyses_new' and at3.deleted= 0)
+
+
+select at3.* from ActionType at3 where at3.code = 'analyses_new' and at3.deleted= 0
+
+
+
+
+
+
+
+select 
+jt.*,
+a.signature,
+at2.code, 
+a.status ,
+at2.name
+-- GROUP_CONCAT(CONCAT(apt.name,": ", case when apt.typeName = 'String' THEN aps.value 
+-- when apt.typeName = 'Reference' THEN apr.value 
+-- when apt.typeName = 'Date' THEN apd.value end) separator '\n')
+from ActionProperty ap 
+left join ActionProperty_String aps using(id) 
+left join ActionProperty_Reference apr using(id)
+left join ActionProperty_Date apd using(id)
+left join ActionProperty_Job_Ticket apjt using(id)
+join Action a on a.id = ap.action_id and a.deleted = 0 and a.event_id = 33796707 
+join (select at2.* from ActionType at2 where at2.deleted =0 and at2.group_id = 42943 union select at3.* from ActionType at2 join ActionType at3 on at2.id = at3.group_id where at2.group_id = 42943) at2 on at2.id = a.actionType_id and at2.deleted = 0
+join ActionPropertyType apt on apt.id = ap.type_id and apt.actionType_id = at2.id-- and apt.typeName != 'JobTicket'
+join Job_Ticket jt on jt.id = apjt.value 
+where ap.deleted = 0
+GROUP by a.id ORDER by a.createDatetime, at2.name, apt.idx
+
+
+select * from Job_Ticket jt ;
+
+select * from rbJobType 
+
+select * from ActionType at2 where at2.id in (select apt.actionType_id from ActionPropertyType apt where apt.valueDomain = '999') and at2.deleted = 0;
+
+
+select jt.* from rbJobType rjt
+join Job j on j.jobType_id = rjt.id
+join Job_Ticket jt on jt.master_id = j.id
+where rjt.code = '999'
+
+
+
+
+
+
+select 
+ttj.datetimeTaken ,
+at3.name ,
+a.id,
+a.takenTissueJournal_id ,
+case when ttj.datetimeTaken 
+-- at2.name ,
+-- a.begDate,
+GROUP_CONCAT(CONCAT(apt.name,": ", case when apt.typeName = 'String' THEN aps.value 
+when apt.typeName = 'Reference' THEN apr.value 
+when apt.typeName = 'Date' THEN apd.value end) separator '\n')
+from ActionProperty ap 
+left join ActionProperty_String aps using(id) 
+left join ActionProperty_Reference apr using(id)
+left join ActionProperty_Date apd using(id)
+join Action a on a.id = ap.action_id and a.deleted = 0 and a.event_id = 33824866 
+join Event e2 on e2.id = a.event_id and e2.deleted = 0
+join ActionType at2 on at2.id = a.actionType_id and at2.deleted = 0
+join ActionType at3 on at3.id = at2.group_id and at3.deleted = 0 
+join ActionPropertyType apt on apt.id = ap.type_id and apt.actionType_id = at2.id and at2.id in (select DISTINCT apt.actionType_id from ActionPropertyType apt where apt.valueDomain REGEXP '999|9767|2-9')--  and apt.name != 'Номерок'
+join TakenTissueJournal ttj on ttj.id = a.takenTissueJournal_id and ttj.client_id = e2.client_id 
+where ap.deleted = 0
+GROUP by a.takenTissueJournal_id, at3.id -- limit 10 
+-- GROUP by a.id ORDER by a.createDatetime, at2.name, apt.idx
+
+select lastName , [1193] FROM Person PIVOT(count(lastName) for id in ([1193])) as pivottable
+
+
+select * from TakenTissueJournal ttj where ttj.id = 65370;
+
+select * from Action where id = 99838563;
+
+
+select * from rbJobType_ActionType rjtat ;
+
