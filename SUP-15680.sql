81edaee274886451311a9aa2a925ff2afef5704c
lameR <erpresents@gmail.com>
2021-03-25 13:03:46 +0300

    SUP-15680 25.03.21

--- SUP-15680.sql
+++ SUP-15680.sql
@@ -1,3 +1,6 @@
-SELECT a.actionType_id from Action a where a.id = 99827557
+SELECT a.actionType_id, a.event_id from Action a where a.id = 99827557
 
 select * from Event e ;
+
+
+select * from rbPrintTemplate where context = 'ane_consult';
