f536243350aa7f6ae9b78aba1e6a16e94e1f6101
lameR <erpresents@gmail.com>
2021-03-26 12:59:54 +0300

    SUP-15792 26.03.21

--- SUP-15792.sql
+++ SUP-15792.sql
@@ -1,34 +1,35 @@
+-- set @return = '';
+set @return = 'кала';
 select count(rep.id) num,
-GROUP_CONCAT(CONCAT_WS(' ',ret.code, ret.name,'retres.idx:',retres.idx,'resper.idx:',resrep.idx) separator '\n') ret, 
-GROUP_CONCAT(CONCAT_WS(' ', retres.id, resrep.id, res.id, res.name, res.description)separator '\n') res, 
-rep.* from rbEpicrisisTemplates ret
+-- GROUP_CONCAT(CONCAT_WS(' ',ret.code, ret.name,'retres.idx:',retres.idx,'resper.idx:',resrep.idx) separator '\n') ret, 
+-- GROUP_CONCAT(CONCAT_WS(' ', retres.id, resrep.id, res.id, res.name, res.description)separator '\n') res, 
+regexp_substr(rep.defaultValue, '(AND at).(name|title).*LIKE.*'),
+rep.* 
+ from rbEpicrisisTemplates ret
 left join rbEpicrisisTemplates_rbEpicrisisSections retres on retres.id_rbEpicrisisTemplates = ret.id 
 left join rbEpicrisisSections res on retres.id_rbEpicrisisSections = res.id 
 left join rbEpicrisisSections_rbEpicrisisProperty resrep on resrep.id_rbEpicrisisSections = res.id 
 left join rbEpicrisisProperty rep on resrep.id_rbEpicrisisProperty = rep.id
 where ret.id = 57 -- and rep.name regexp 'кров'
-and res.name = 'За время пребывания в ЦРБ проведены обследования' and rep.`type` != 9
+and res.name = 'За время пребывания в ЦРБ проведены обследования' and rep.`type` != 9 and rep.defaultValue regexp @return
 group by res.id, rep.id
-order by  retres.idx, resrep.idx;
+order by rep.name ;-- retres.idx, resrep.idx;
+
+select a2.code, a2.name, a2.title, a.* from action a
+  join actiontype a2 on a2.id = a.actionType_id and class = 1 and a2.name regexp @return
+  where a.event_id = 6137807 and a.deleted = 0 order by a2.title 
 
 
-
-
-select * from rbepicrisistemplates r ;
-
-
-SELECT  
-  IFNULL(DATE_FORMAT(a.endDate,'%d.%m.%Y %h:%i'),'ДАТА НЕ УКАЗАНА') AS 'Дата',
+ 
+  SELECT  
+  at.name , at.title ,
+  IFNULL(DATE_FORMAT(a.endDate,'%d.%m.%Y'),'ДАТА НЕ УКАЗАНА') AS 'Дата',
   apt.name 'Фактор',
   aps.value AS 'Результат',
-  ap.norm 'Норма'
-   ,IF(ap.norm LIKE 'Отрицательный', IF(CAST(REPLACE(aps.value,',','.') AS DECIMAL(6,2))>0,'Без отклонений','Замечено отклонение от нормы'),
-     IF(CAST(REPLACE(aps.value,',','.') AS DECIMAL(6,2)) BETWEEN
-               CAST(REPLACE(SUBSTRING_INDEX(ap.norm,' ',1),',','.') AS DECIMAL(6,2)) 
-               AND CAST(REPLACE(SUBSTRING_INDEX(ap.norm,' ',-1),',','.') AS DECIMAL(6,2)),'Без отклонений','Замечено отклонение от нормы')) 'Отклонение от нормы'  
+  apt.norm 'Норма' 
   FROM Event e
   INNER JOIN Action a ON a.event_id=e.id AND a.deleted=0
-  INNER JOIN ActionType at ON a.actionType_id=at.id AND at.deleted=0 AND at.class=1  AND at.title LIKE 'Анализ мочи'
+  INNER JOIN ActionType at ON a.actionType_id=at.id AND at.deleted=0 AND at.class=1 AND (at.name regexp @return or at.title regexp @return)
   INNER JOIN ActionPropertyType apt ON apt.actionType_id=at.id AND apt.deleted=0 AND apt.typeName NOT LIKE 'JobTicket'
   INNER JOIN ActionProperty ap ON ap.action_id=a.id AND ap.type_id=apt.id
   LEFT JOIN ActionProperty_String aps ON ap.id=aps.id
@@ -36,20 +37,44 @@ SELECT
   LEFT JOIN ActionProperty ap_date ON ap_date.action_id=a.id AND ap_date.type_id=apt_date.id AND ap_date.deleted=0
   LEFT JOIN ActionProperty_Job_Ticket  apjt_date ON ap_date.id=apjt_date.id 
   LEFT JOIN Job_Ticket jt ON apjt_date.value=jt.id
-  WHERE
-  e.id=%s
-  ORDER BY a.endDate, 2,1 ASC 
-
-
-
-
-  
-  
-  select a2.code, a2.name, a2.title, a.* from action a
-  join actiontype a2 on a2.id = a.actionType_id-- and a2.name regexp 'мочи'
-  where a.event_id = 6137807 and a.deleted = 0
-  
-  
-  select * from actiontype a where a.name = 'Общий (клинический) анализ крови';
+--   WHERE
+--   e.id=6137807
+  ORDER BY a.endDate, 2,1 ASC
+-- 
+-- select * from rbepicrisistemplates r ;
+-- 
+-- SELECT  
+--   IFNULL(DATE_FORMAT(a.endDate,'%d.%m.%Y'),'ДАТА НЕ УКАЗАНА') AS 'Дата',
+--   apt.name 'Фактор',
+--   aps.value AS 'Результат',
+--   apt.norm 'Норма'
+-- --   ,IF(apt.norm LIKE 'Отрицательный', IF(CAST(REPLACE(aps.value,',','.') AS DECIMAL(6,2))>0,'Без отклонений','Замечено отклонение от нормы'),
+-- --     IF(CAST(REPLACE(aps.value,',','.') AS DECIMAL(6,2)) BETWEEN
+-- --               CAST(REPLACE(SUBSTRING_INDEX(apt.norm,' ',1),',','.') AS DECIMAL(6,2)) 
+-- --               AND CAST(REPLACE(SUBSTRING_INDEX(apt.norm,' ',-1),',','.') AS DECIMAL(6,2)),'Без отклонений','Замечено отклонение от нормы')) 'Отклонение от нормы'  
+--   FROM Event e
+--   INNER JOIN Action a ON a.event_id=e.id AND a.deleted=0
+--   INNER JOIN ActionType at ON a.actionType_id=at.id AND at.deleted=0 AND at.class=1 AND at.name LIKE 'Клинический анализ кала'
+--   INNER JOIN ActionPropertyType apt ON apt.actionType_id=at.id AND apt.deleted=0 AND apt.typeName NOT LIKE 'JobTicket'
+--   INNER JOIN ActionProperty ap ON ap.action_id=a.id AND ap.type_id=apt.id
+--   LEFT JOIN ActionProperty_String aps ON ap.id=aps.id
+--   LEFT JOIN ActionPropertyType apt_date ON apt_date.actionType_id=at.id AND apt_date.deleted=0 AND apt_date.typeName LIKE 'JobTicket'
+--   LEFT JOIN ActionProperty ap_date ON ap_date.action_id=a.id AND ap_date.type_id=apt_date.id AND ap_date.deleted=0
+--   LEFT JOIN ActionProperty_Job_Ticket  apjt_date ON ap_date.id=apjt_date.id 
+--   LEFT JOIN Job_Ticket jt ON apjt_date.value=jt.id
+--   WHERE
+--   e.id=%s
+--   ORDER BY a.endDate, 2,1 ASC
+-- 
+-- 
+-- 
+--   
+--   
+--   select a2.code, a2.name, a2.title, a.* from action a
+--   join actiontype a2 on a2.id = a.actionType_id and class = 1-- and a2.name regexp 'мочи'
+--   where a.event_id = 6137807 and a.deleted = 0 order by a2.title 
+--   
+--   
+--   select * from actiontype a where a.name = 'Общий (клинический) анализ крови';
   
   
