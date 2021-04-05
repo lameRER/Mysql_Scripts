b6ae228d6763cfdcc9ae747e3e5f67bcea6d4c5e
lameR <erpresents@gmail.com>
2021-04-05 09:39:23 +0300

    SUP-16076 05.04.21

--- SUP-16076.sql
+++ SUP-16076.sql
@@ -7,3 +7,20 @@ select * from rbPrintTemplate rpt where rpt.context = (select at2.context from A
 select * from ActionType at2 where at2.name REGEXP 'ОНМК';
 
 select * from ActionPropertyType apt where apt.actionType_id = 43361;
+
+
+
+select * from DestinationTree_ActionType where actionType_id = 43361;
+
+
+select * from OrgStructure_ActionType where actionType_id = 43361;
+
+
+
+
+
+select * from rbPrintTemplate rpt where rpt.context in (select context from ActionType  where group_id = 14285 order by name)
+
+
+
+select * from rbPrintTemplate rpt where rpt.context in (select at2.context from ActionType at2 where at2.name REGEXP 'ОНМК');
