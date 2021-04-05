81058f0ae57db1ee255ac65d291086c8d05b1aa6
lameR <erpresents@gmail.com>
2021-04-05 18:13:26 +0300

    SUP-15431

--- CHE/SUP-15431.sql
+++ CHE/SUP-15431.sql
@@ -1,5 +1,11 @@
-Select * From medialog.COMPLICATIONS4 c
+Select * From medialog.COMPLICATIONS4 c;
+
+
+set @return = 190;
+Select * From medialog.COMPLICATIONS4 c where c.COMPLICATIONS4_ID = @return or c.PARENT = @return;
+
+select * from rbThesaurus rt where rt.code REGEXP '^35-0';
 
 
 
-select * from rbAnesthesia ra ;
+
