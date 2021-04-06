203d771e04464a625e714d39c119bebaf4e9097e
lameR <erpresents@gmail.com>
2021-04-06 17:06:59 +0300

    SUP-15871 06.04.21

--- SUP-15810.sql
+++ SUP-15810.sql
@@ -2,4 +2,4 @@ select * from rbepicrisistemplates r ;
 
 
 
-select * from actiontype a where a.name regexp '^Выписной эпикриз' and deleted = 0;
+select * from actiontype a where a.name regexp '^Выписной эпикриз' and deleted = 0;	
