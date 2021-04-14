91c5fa92916643730eee74c607a0b8791dfc9dd1
lameR <erpresents@gmail.com>
2021-04-14 14:00:02 +0300

    14-04-2021_14:00

--- Script-4.sql
+++ Script-4.sql
@@ -1,7 +1,22 @@
 select * from Event e where e.client_id = 519681;
 
 
-select * from Action a where a.event_id = 33834573 ;
-
+select at2.name, a.status, a.plannedEndDate,  a.* from 
+(select a.* from Action a 
+join Action a1 on a1.id = a.parent_id 
+where a.event_id = 33796707 and a.parent_id is not null
+union 
+select a1.* from Action a 
+join Action a1 on a1.id = a.parent_id 
+where a.event_id = 33796707 and a.parent_id is not null) as a
+join ActionType at2 on at2.id = a.actionType_id
+order by a.id 
 
 select e.externalId from Event e where e.id = 33834573;
+
+
+select a.* from Action a  
+where a.event_id = 33834573 and a.parent_id is not null
+
+
+select * from Action ;
