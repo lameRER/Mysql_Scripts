3432f572abc3cf8dd766aefd67bc00e352c21b41
lameR <erpresents@gmail.com>
2021-04-16 13:00:01 +0300

    16-04-2021_13:00

--- SUP-16176.sql
+++ SUP-16176.sql
@@ -3,7 +3,11 @@ join ActionPropertyType apt on apt.actionType_id = at2.id and apt.deleted = 0 an
 where at2.group_id = 12453 and at2.deleted = 0
 
 
+select at2.* from ActionType at2 
+where at2.group_id = 12453 and at2.deleted = 0
 
+
+select * from rbPrintTemplate rpt where rpt.context = 'osmotr';
 
 select ats.* from ActionType_Service ats 
 join ActionType at2 on at2.id = ats.master_id and at2.deleted = 0 and at2.group_id = 12453
---
+++ SUP-16537.sql
@@ -0,0 +1,7 @@
+select * from rbPrintTemplate rpt where rpt.id = 1121;
+
+
+select * from ActionType at2 where at2.context = '1-отд-3';
+
+
+select * from ActionPropertyType apt where apt.actionType_id = 43576 and apt.deleted = 0 order by apt.idx;
