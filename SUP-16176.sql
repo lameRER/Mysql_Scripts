62b16932b583bcd4ea55c19dfc75e67d52527f4f
lameR <erpresents@gmail.com>
2021-04-16 11:08:01 +0300

    16-04-2021_11:08

--- SUP-16176.sql
+++ SUP-16176.sql
@@ -1,3 +1,9 @@
 select at2.code, at2.name, apt.* from ActionType at2 
 join ActionPropertyType apt on apt.actionType_id = at2.id and apt.deleted = 0 and apt.name REGEXP 'Номерок'-- '^Заключение$|^Описание$|^Результат$'
 where at2.group_id = 12453 and at2.deleted = 0
+
+
+
+
+select ats.* from ActionType_Service ats 
+join ActionType at2 on at2.id = ats.master_id and at2.deleted = 0 and at2.group_id = 12453
