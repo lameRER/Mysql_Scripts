a70ac800054cf4968579e158da6f0b406b47b929
lameR <erpresents@gmail.com>
2021-04-19 14:00:01 +0300

    19-04-2021_14:00

--- SUP-16415.sql
+++ SUP-16415.sql
@@ -9,3 +9,7 @@ select * from DestinationTree_ActionType dtat where actionType_id = 11021;
 
 
 select * from DestinationTree_ActionType where master_id = 10082;
+
+
+
+select * from ActionType at2 where at2.group_id = 11021;
