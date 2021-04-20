a81c0c28b28923dda537d205c45b90e2af48ceba
lameR <erpresents@gmail.com>
2021-04-20 11:00:01 +0300

    20-04-2021_11:00

--- SUP-15431.sql
+++ SUP-15431.sql
@@ -9,3 +9,10 @@ select * from rbThesaurus rt where rt.code REGEXP '^35-0';
 
 
 
+select * from ActionType at2 where at2.name REGEXP 'Протокол операции|выписной эпикриз' and at2.deleted = 0;
+
+select * from ActionPropertyType apt where apt.actionType_id = 13723;
+
+
+
+
