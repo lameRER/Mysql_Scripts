f270587ba11441e5f58bd7b989e7fb8768e93aa1
lameR <erpresents@gmail.com>
2021-04-21 18:00:01 +0300

    21-04-2021_18:00

--- SUP-16415.sql
+++ SUP-16415.sql
@@ -13,3 +13,11 @@ select * from DestinationTree_ActionType where master_id = 10082;
 
 
 select * from ActionType at2 where at2.group_id = 11021;
+
+
+
+
+select at2.* 
+from ActionPropertyType apt, ActionType at2, rbJobType rjt 
+where at2.id in (11021,11028,11025,11019) and apt.actionType_id = at2.id and apt.deleted = 0 and apt.name = 'Номерок'
+and rjt.code = apt.valueDomain 
