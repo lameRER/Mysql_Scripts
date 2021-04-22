17bfaeb0264e518f238d42ee2bba5b8155aa8c08
lameR <erpresents@gmail.com>
2021-04-22 18:00:01 +0300

    22-04-2021_18:00

--- SUP-16415.sql
+++ SUP-16415.sql
@@ -24,3 +24,32 @@ and rjt.code = apt.valueDomain
 
 
 select * from OrgStructure_ActionType where actionType_id in(11019,11025,11021,11028)
+
+
+
+
+select * from OrgStructure_Job osj ;
+
+
+select * from Job j ;
+
+select jt.* from Job_Ticket jt 
+join Job j on j.id = jt.master_id and j.jobType_id = 90;
+
+
+;
+
+select * from rbUserProfile rup ;
+
+select rup.name, p.* from Person p 
+join rbUserProfile rup on rup.id = p.userProfile_id ;
+
+select * from Person p where p.login regexp 'Виста';
+
+
+select * from Person_UserProfile pup where pup.person_id = 564;
+
+
+INSERT INTO s11.rbUserProfile
+(id, createDatetime, createPerson_id, modifyDatetime, modifyPerson_id, code, name)
+VALUES(35, '0000-00-00 00:00:00', NULL, '0000-00-00 00:00:00', NULL, 'diag_doctor', 'Врач ИД');
