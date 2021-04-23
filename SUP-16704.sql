0712993a1f95d2f71eb35fd8f43e6a58c9fadc4c
lameR <erpresents@gmail.com>
2021-04-23 17:00:02 +0300

    23-04-2021_17:00

---
+++ SUP-16704.sql
@@ -0,0 +1,3 @@
+select l.* from Visit v, logger.Login l, Person p2 where v.modifyPerson_id = l.person_id 
+
+
---
+++ SUP-16713.sql
@@ -0,0 +1,8 @@
+select * from rbUserProfile rup 
+
+
+select * from rbUserProfile_Right where master_id  = 7
+
+
+
+select * from rbUserRight where name REGEXP 'имеет право';
