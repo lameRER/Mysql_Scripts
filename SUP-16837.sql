ed4c4c0547cc74ec8d84b88a835a3ce909f83cbf
lameR <erpresents@gmail.com>
2021-04-29 16:00:01 +0300

    29-04-2021_16:00

--- SUP-16837.sql
+++ SUP-16837.sql
@@ -1,4 +1,4 @@
-select apt.* from ActionType at2, ActionPropertyType apt 
+select at2.* from ActionType at2, ActionPropertyType apt 
 where at2.group_id in (select at3.id from ActionType at3 where at3.code in('КТ - уКРТбк','КТ- уКРТом'))
 and apt.actionType_id = at2.id
 
@@ -27,3 +27,10 @@ set @number = 11;
 INSERT into OrgStructure_ActionType (master_id, idx, actionType_id)
 select DISTINCT osat.master_id, osat.idx, at2.id actionType_id from ActionType at2, OrgStructure_ActionType osat 
 where at2.group_id in (select at3.id from ActionType at3 where at3.code in('КТ - уКРТбк','КТ- уКРТом')) and osat.master_id = 139
+
+
+select * from ActionType ;
+select * from ActionPropertyType where deleted = 0 and actionType_id in (select id from ActionType where group_id =26227 ) group by name;
+
+
+select * from ActionType at2 where group_id = 42802;
