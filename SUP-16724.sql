5a4087024a3a31e60b68af8ee1c1ef80a570aaff
lameR <erpresents@gmail.com>
2021-05-04 11:00:01 +0300

    04-05-2021_11:00

--- SUP-16724.sql
+++ SUP-16724.sql
@@ -1,6 +1,13 @@
-select a.* from ACTION a, actiontype a2
-where a.actionType_id = a2.id and a.deleted = 0 and a2.deleted = 0 and a2.flatCode = 'leaved' and a.begDate != a.endDate 
-order by a.id desc 
+select a.* from Action a, actiontype a2
+where a.actionType_id = a2.id and a.deleted = 0 and a2.deleted = 0 and a2.flatCode = 'leaved' and a.begDate >= '2021-04-12 08:00' and a.begDate <= '2021-04-12 09:00'
+order by a.endDate desc 
 
 
 select * from actiontype a where a.flatCode != '';
+
+
+
+select * from action a where a.begDate = '2021-04-12' and a.deleted = 0
+
+
+select * from ;
--- Script-29.sql
+++ Script-29.sql
@@ -1,1 +1,6 @@
 select * from ActionType at2 order by id desc;
+
+
+select * from Action a
+join ActionType at2 on at2.id = a.ActionType_id
+join ActionType_Service ats on ats.master_id = at2.id
