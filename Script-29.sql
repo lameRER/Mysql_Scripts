e770e68c3eaa4bd5dac265a3c5be30a30dfdaa7b
lameR <erpresents@gmail.com>
2021-05-05 15:00:01 +0300

    05-05-2021_15:00

--- Script-29.sql
+++ Script-29.sql
@@ -6,4 +6,4 @@ join ActionType at2 on at2.id = a.ActionType_id
 join ActionType_Service ats on ats.master_id = at2.id
 
 
-select * from rbJobType rjt ;
+select * from rbJobType rjt ;		
---
+++ Script-30.sql
@@ -0,0 +1,3 @@
+select * from ActionType at2 WHERE  at2.name REGEXP 'радиовизиография';
+
+select * from ActionType_Service ats where ats.master_id in(9346);
