1a72b8e66bf7f13460493f20ab12ce33349943c3
lameR <erpresents@gmail.com>
2021-05-07 11:00:10 +0300

    07-05-2021_11:00

--- Script-31.sql
+++ Script-31.sql
@@ -1,1 +1,6 @@
 select * from rbPrintTemplate rpt where rpt.name REGEXP 'договор';
+
+
+SELECT e.client_id, a.* FROM  Event e INNER JOIN Action a ON e.id = a.event_id
+WHERE e.eventType_id = 29 
+AND a.person_id = 1924 AND a.directionDate LIKE '%2021-05-07%';
