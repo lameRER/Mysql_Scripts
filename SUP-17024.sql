adc5c430eb5b26531864a12f69f805791ff36485
lameR <erpresents@gmail.com>
2021-05-07 10:00:01 +0300

    07-05-2021_10:00

--- SUP-17024.sql
+++ SUP-17024.sql
@@ -7,3 +7,7 @@ select * from ActionType at2 where at2.name REGEXP 'липидограмма' an
 select * from OrgStructure_Job where jobType_id = 59
 
 select * from OrgStructure_ActionType where master_id in(2,158);
+
+
+select * from ActionPropertyType apt where apt.actionType_id in(10095,11074);
+
