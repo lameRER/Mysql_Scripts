d9b5428641e1854db55497fe02f9a515500ecd97
lameR <erpresents@gmail.com>
2021-05-13 12:00:01 +0300

    13-05-2021_12:00

--- CHE-323.sql
+++ CHE-323.sql
@@ -1,1 +1,19 @@
 SELECT * from rbPrintTemplate rpt where rpt.name REGEXP 'Согласия' and rpt.context ='token'
+
+
+
+INSERT into rbPrintTemplate (createDatetime, createPerson_id, modifyDatetime, modifyPerson_id, code, name, context, fileName, `default`, dpdAgreement, `type`, hideParam, banUnkeptDate, counter_id, deleted, isPatientAgreed, groupName, documentType_id, isEditableInWeb, chkProfiles, chkPersons, pageOrientation, sendMail) 
+select 
+NOW() createDatetime, null createPerson_id, NOW() modifyDatetime, null modifyPerson_id, 
+code, CONCAT(name, ' (Выбор)'), context, fileName, `default`, dpdAgreement, `type`, hideParam, banUnkeptDate, counter_id, deleted, isPatientAgreed, groupName, documentType_id, isEditableInWeb, chkProfiles, chkPersons, pageOrientation, sendMail from rbPrintTemplate where id = 1314 ;
+
+
+
+
+select * from rbPrintTemplate rpt order by id desc;
+
+
+
+
+
+
