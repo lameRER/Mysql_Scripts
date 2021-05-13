282f90d39c6b41b391aae685c42f9d064e5e0200
lameR <erpresents@gmail.com>
2021-05-13 18:00:01 +0300

    13-05-2021_18:00

--- SUP-17146.sql
+++ SUP-17146.sql
@@ -1,10 +1,42 @@
-select * from ActionType at2 where at2.name REGEXP '^консультация врача-тра';
+select at2.* from ActionType at2 where at2.name REGEXP '^консультация врача-тра';
 
 56181
 
 select * from DestinationTree_ActionType where actionType_id =56181
 
+select * from ActionType at2 where at2.name  ='Консультация ЛОР';
 
+
+select * from ActionPropertyType apt where apt.actionType_id in(40709,56181);
+
+
+select * from OrgStructure os order by os.id DESC ;
+
+select * from OrgStructure_ActionType osat where osat.master_id =209;
+
+select * from rbJobType_ActionType where master_id = 190;
+
+select * from OrgStructure_Job osj where osj.master_id = 68;
+
+select * from OrgStructure_ActionType osat where master_id = 68;
+
+
+select * from rbJobType rjt where code REGEXP '6-2|2-17';
+
+select * from OrgStructure_Job where master_id = 68;
+
+INSERT into OrgStructure_Job (master_id, idx, jobType_id, begTime, endTime, quantity, lastAccessibleDate, isVisibleInDR, person_id)
+select osj.master_id, 1 idx, 190 jobType_id, osj.begTime, osj.endTime, osj.quantity, osj.lastAccessibleDate, osj.isVisibleInDR, osj.person_id from OrgStructure_Job osj where osj.master_id = 68;
+
+INSERT into rbJobType (createDatetime, createPerson_id, modifyDatetime, modifyPerson_id, group_id, code, regionalCode, name, listContext, actionStatusChanger, actionPersonChanger, actionDateChanger, ticketDuration, showOnlyPayed, durationVisibility, ticketAssignWay, quotingMode, instrumental, jobStatusModifier)
+select 
+NOW() createDatetime, null  createPerson_id,NOW() modifyDatetime, null modifyPerson_id, 
+group_id, '2-16' code, regionalCode, (select at2.name from ActionType at2 where at2.name REGEXP '^консультация врача-тра') name, listContext, actionStatusChanger, actionPersonChanger, actionDateChanger, ticketDuration, showOnlyPayed, durationVisibility, ticketAssignWay, quotingMode, instrumental, jobStatusModifier from rbJobType where id =92
+
+
+select * from rbJobType rjt ;
+
+-- insert into ActionPropertyType (deleted, actionType_id, idx, template_id, name, shortName, descr, unit_id, typeName, valueDomain, defaultValue, isVector, norm, sex, age, penalty, penaltyUserProfile, visibleInJobTicket, visibleInTableRedactor, isAssignable, test_id, defaultEvaluation, canChangeOnlyOwner, isActionNameSpecifier, laboratoryCalculator, inActionsSelectionTable, redactorSizeFactor, isFrozen, typeEditable, visibleInDR, userProfile_id, userProfileBehaviour, copyModifier, isVitalParam, vitalParamId, isODIIParam, ticketsNeeded, customSelect, autoFieldUserProfile, formulaAlias)
 select * from ActionPropertyType apt where apt.actionType_id = 56181 and apt.deleted = 0 order by apt.idx ;
 
 select * from DestinationTree where deleted= 0;
