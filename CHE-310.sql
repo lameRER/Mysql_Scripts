1f720ba66a0485cf8b69e00966f78594ae11a106
lameR <erpresents@gmail.com>
2021-05-14 17:00:01 +0300

    14-05-2021_17:00

---
+++ CHE-310.sql
@@ -0,0 +1,13 @@
+select * from rbCounter rc ;
+
+
+select * from DestinationTree_ActionType dtat where dtat.master_id  = 9748;
+
+select * from DestinationTree dt where dt.deleted = 0 ;
+
+
+select * from ActionPropertyType apt where apt.actionType_id = 21259;
+
+
+
+select DISTINCT apt.typeName from ActionPropertyType apt group by apt.typeName ;
