369e315925e467d31f8d7907a06a72ae14f17e2f
lameR <erpresents@gmail.com>
2021-05-14 19:00:05 +0300

    14-05-2021_19:00

--- CHE-310.sql
+++ CHE-310.sql
@@ -6,7 +6,7 @@ select * from DestinationTree_ActionType dtat where dtat.master_id  = 9748;
 select * from DestinationTree dt where dt.deleted = 0 ;
 
 
-select * from ActionPropertyType apt where apt.actionType_id = 21259;
+select * from ActionPropertyType apt where apt.actionType_id = 21259 and deleted  = 0;
 
 
 
