35719e67e5a44d647397f18f529010329aefac9c
lameR <erpresents@gmail.com>
2021-05-17 18:00:01 +0300

    17-05-2021_18:00

--- SUP-17026.sql
+++ SUP-17026.sql
@@ -66,7 +66,7 @@ where not EXISTS (select * from rbVitalParams rvp where rvp.code = tmp.code)
 
 
 select apt.isVitalParam, apt.vitalParamId, apt.* from ActionPropertyType apt where apt.actionType_id in (select at2.id from ActionType at2 where at2.group_id =56182) and apt.isVitalParam = 0 ORDER by apt.actionType_id, apt.idx ;
-select * from rbVitalParams where code ='613' ;
+select * from rbVitalParams where code ='608';
 
 ;
 
