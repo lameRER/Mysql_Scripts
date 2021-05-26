a152c616a796d5dea303f0c6de5d1dfb7b70f5c7
lameR <erpresents@gmail.com>
2021-05-26 10:30:01 +0300

    26-05-2021_10:30

--- SUP-17467.sql
+++ SUP-17467.sql
@@ -1,1 +1,7 @@
-select * from person p 	
+select * from person p where p.userProfile_id = 1
+
+
+select * from orgstructure_job oj where oj.jobType_id = 5
+
+
+select * from rbjobtype r where r.code = '3'
