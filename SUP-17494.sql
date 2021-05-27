401215cc9234843ecc038368621d9bc756f09fd5
lameR <erpresents@gmail.com>
2021-05-27 16:10:02 +0300

    27-05-2021_16:10

--- SUP-17494.sql
+++ SUP-17494.sql
@@ -7,5 +7,14 @@ select *
 from ActionPropertyType apt where apt.isVitalParam = 1;
 
 select *
-from ActionType where id = 43639
+from ActionType where id = 49887
 ;
+
+
+select *
+from ActionPropertyType where actionType_id= 49887 and deleted= 0;
+
+select *
+from rbPrintTemplate where context = '1193-26179_1';
+
+
