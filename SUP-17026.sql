87f101542bf9b7c485932df0aebf5c54f149bf2b
lameR <erpresents@gmail.com>
2021-05-27 10:30:01 +0300

    27-05-2021_10:30

--- SUP-17026.sql
+++ SUP-17026.sql
@@ -172,4 +172,6 @@ select * from ActionPropertyType apt where apt.actionType_id in (select osat.act
 select * from ActionPropertyType apt where apt.actionType_id in (select osat.actionType_id from OrgStructure_ActionType osat where osat.master_id = 72) and apt.deleted = 0;
 
 
+select * from information_schema.PROCESSLIST p where COMMAND != 'Sleep';
+
 
