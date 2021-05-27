8c0b2c6798a423a9e6cab7f94c538640d0897e7a
lameR <erpresents@gmail.com>
2021-05-27 12:20:01 +0300

    27-05-2021_12:20

--- SUP-16164.sql
+++ SUP-16164.sql
@@ -110,14 +110,14 @@ os.name as OperatingRoom,
 '' as Anestes,
 '' as OperBrig,
 '' as GrBlood
-from Event e 
+from Event e
 join Client c on c.id = e.client_id and c.deleted = 0
 left join Diagnostic d2 on d2.event_id = e.id and d2.deleted = 0 and d2.id = (select max(d.id) from Diagnostic d where d.event_id = e.id)
 left join Diagnosis d3 on d3.id = d2.diagnosis_id and d3.deleted = 0
 join rbDiagnosisType rdt on d2.diagnosisType_id = rdt.id
 join `Action` a on a.event_id = e.id and a.deleted = 0 and a.status != 3 and a.specifiedName != ''
 left join JsonData jd on jd.id REGEXP a.id 
-left join OrgStructure os on os.id = REPLACE(STRINGDECODE(urldecoder(jd.json)), '.*"table":.?"(\\d+)".*', '\\1')
+left join OrgStructure os on os.id = REPLACE(STRINGDECODE(urldecoder(jd.json)), '.*\"table\":.?\"(\\d+)\".*', '\\1')
 -- left join OrgStructure os on os.id = REGEXP_REPLACE(STRINGDECODE(urldecoder(jd.json)), '.*"table":.?"(\\d+)".*', '\\1')
 WHERE e.eventType_id = 94 and e.deleted = 0	
 ORDER by os.name, a.plannedEndDate
