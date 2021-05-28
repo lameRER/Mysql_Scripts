1662e7f03ce713c33bf884150b78047e871d85cd
lameR <erpresents@gmail.com>
2021-05-28 17:50:01 +0300

    28-05-2021_17:50

--- SUP-17572.sql
+++ SUP-17572.sql
@@ -3,10 +3,11 @@ select *
 from Organisation where id =3188;
 select *
 from Client where id =1549863;
-select a.*
+select at2.name, e.client_id, a.*
 from Event e
-join Action a on a.event_id = e.id and a.actionType_id in(14181,14247,14258,14414)
-where e.client_id= 1549863 ;
+join Action a on a.event_id = e.id
+join ActionType at2 on at2.id = a.actionType_id and at2.id =14173
+where e.client_id in(1549863,1552463) ;
 select *
 from ActionType where name regexp 'cov';
 select *
