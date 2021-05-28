d025fd56e0084475c39aa58a9ae45218e62596ca
lameR <erpresents@gmail.com>
2021-05-28 09:50:01 +0300

    28-05-2021_09:50

--- SUP-17494.sql
+++ SUP-17494.sql
@@ -1,38 +1,55 @@
 select *
-from rbPrintTemplate where name regexp '';
+from rbPrintTemplate
+where name regexp '';
 
 
 
 select *
-from ActionPropertyType apt where apt.isVitalParam = 1;
+from ActionPropertyType apt
+where apt.isVitalParam = 1;
 
 select *
-from DestinationTree_ActionType where actionType_id in (
-select id
-from ActionType where name regexp 'cov')
+from DestinationTree_ActionType
+where actionType_id in (
+    select id
+    from ActionType
+    where name regexp 'cov')
 ;
 
 
 select *
-from DestinationTree where id =10070;
+from DestinationTree
+where id = 10070;
 
 
 select *
-from ActionType where id in(49882,49883,49886,49887,49885);
+from ActionType
+where id in (49882, 49883, 49886, 49887, 49885);
 
 
 select *
-from rbPrintTemplate where context= 'SARS';
+from rbPrintTemplate
+where context = 'SARS';
 
 
 
 select context
-from ActionType WHERE ID = 49901;
+from ActionType
+WHERE ID = 49901;
+
 
 select *
-from ActionPropertyType where actionType_id= 49901 and deleted= 0;
+from ActionType where context = 'SARS';
+
+select * from ActionPropertyType where actionType_id=49885 and deleted= 0
 
 select *
-from rbPrintTemplate where context = '145-9957';
+from ActionPropertyType
+where actionType_id = 49901
+  and deleted = 0;
+
+select *
+from rbPrintTemplate
+where context = '145-9957';
 
 
