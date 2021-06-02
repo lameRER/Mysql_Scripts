5c7643f487e02922392d6737f607705c961d0b43
lameR <erpresents@gmail.com>
2021-06-02 15:50:01 +0300

    02-06-2021_15:50

---
+++ test.sql
@@ -0,0 +1,16 @@
+select *
+from ActionType
+where name regexp 'qr
+';
+
+
+select *
+
+from Action where  actionType_id = 11468 and createPerson_id =614
+select *
+from Person where login = 'виста';
+
+
+select *
+from Organisation where id = 3188
+;
