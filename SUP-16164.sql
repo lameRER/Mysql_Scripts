c9e147963ea6f6b469b62f0290bcc92495f34ef2
lameR <erpresents@gmail.com>
2021-06-02 10:50:01 +0300

    02-06-2021_10:50

--- SUP-16164.sql
+++ SUP-16164.sql
@@ -10,7 +10,7 @@ concat(m.DiagID, '-', m.BlockName) as Diagnosis,
 a.specifiedName as Operation,
 os.name as OperatingRoom,
 aps.value as Anestes,
-CONCAT_WS('\n',
+CONCAT_WS('\n\n',
     if(p.lastName is not null, concat('Ответственный за переливание крови: ', concat_ws(' ', p.lastName, concat(left(p.firstName, 1), '.', left(p.patrName, 1), '.'))), NULL),
     if(p1.lastName is not null, concat('Дежурный по оперблоку: ', concat_ws(' ', p1.lastName, concat(left(p1.firstName, 1), '.', left(p1.patrName, 1), '.'))), NULL),
     if(p2.lastName is not null, concat('Ассистенты: ', concat_ws(' ', p2.lastName, concat(left(p2.firstName, 1), '.', left(p2.patrName, 1), '.'))), NULL)
