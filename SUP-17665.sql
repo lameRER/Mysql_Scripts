1cec2c26f2f78a5b22051dea1f69aa949b2c5091
Vista <lustik_alex@nilc.spb.ru>
2021-06-07 15:02:01 +0300

    07-06-2021_15:02

--- SUP-17665.sql
+++ SUP-17665.sql
@@ -12,8 +12,8 @@ Join ActionProperty_String aps using(id)
 join Action a on ap.action_id = a.id and a.deleted = 0
 join ActionType at2 on at2.id = a.actionType_id and at2.deleted = 0 and at2.flatCode = 'received'
 join ActionPropertyType apt on apt.actionType_id = at2.id and apt.deleted =0 and ap.type_id = apt.id and apt.name = 'Доставлен'
-where ap.deleted= 0
+where ap.deleted= 0;
 
 
 select *
-from ActionProperty_String;
+from ActionProperty_String_copy_07_06_21;
