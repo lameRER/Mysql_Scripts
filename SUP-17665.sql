d3f66b5315fb436828ef7582694a0bb8e88531c7
Vista <lustik_alex@nilc.spb.ru>
2021-06-07 17:40:01 +0300

    07-06-2021_17:40

--- SUP-17665.sql
+++ SUP-17665.sql
@@ -14,7 +14,7 @@ from netricaTypeFromDiseaseStart;
 # позднее 24-х часов
 
 
-
+insert into ActionProperty_Reference(id, `index`, value)
 select aps.id, 0,
        case
            when aps.value regexp '^в первые 6часов|^1 час|^2 часа|^3 часа|^4|^4 часа|^5 часов|^6 часов|^< font style="vertical-align: inherit;">< font style="vertical-align: inherit;">1 час< /font>< /font>|^< font style="vertical-align: inherit;">< font style="vertical-align: inherit;">4 часа< /font>< /font>' then 4
@@ -27,7 +27,13 @@ join Action a on ap.action_id = a.id and a.deleted = 0
 join ActionType at2 on at2.id = a.actionType_id and at2.deleted = 0 and at2.flatCode = 'received'
 join ActionPropertyType apt on apt.actionType_id = at2.id and apt.deleted =0 and ap.type_id = apt.id and apt.name = 'Доставлен'
 where ap.deleted= 0 and aps.id is not null; -- group by aps.value;
+select *
+from ActionPropertyType where actionType_id = 15084;
 
+
+
+select *
+from ActionProperty_String where id = 222718137;
 
 select *
 from ActionProperty_String_copy_07_06_21;
