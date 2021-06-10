ef4a16b2295b4f4f4570360e2fe7281e398edbed
Lustik_ak <lustik_alex@nilc.spb.ru>
2021-06-10 18:10:02 +0300

    10-06-2021_18:10

--- CHE-348.sql
+++ CHE-348.sql
@@ -61,11 +61,11 @@ SELECT
     LEFT JOIN ActionProperty ap10 ON a.id = ap10.action_id AND apt10.id = ap10.type_id AND ap10.deleted = 0
     LEFT JOIN ActionProperty_OrgStructure apr ON ap.id = apr.id
     LEFT JOIN ActionProperty_Integer api ON ap3.id = api.id
-    LEFT JOIN ActionProperty_Reference apr1 ON apr1.id = ap1.id
-    LEFT JOIN ActionProperty_Reference apr2 ON apr2.id = ap2.id
-    LEFT JOIN ActionProperty_Reference apr3 ON apr3.id = ap4.id
-    LEFT JOIN ActionProperty_Reference apr4 ON apr4.id = ap5.id
-    LEFT JOIN ActionProperty_Reference apr5 ON apr5.id = ap6.id
+    LEFT JOIN ActionProperty_Person apr1 ON apr1.id = ap1.id
+    LEFT JOIN ActionProperty_Person apr2 ON apr2.id = ap2.id
+    LEFT JOIN ActionProperty_Person apr3 ON apr3.id = ap4.id
+    LEFT JOIN ActionProperty_Person apr4 ON apr4.id = ap5.id
+    LEFT JOIN ActionProperty_Person apr5 ON apr5.id = ap6.id
     LEFT JOIN ActionProperty_HospitalBed aphb ON ap7.id = aphb.id
     LEFT JOIN ActionProperty_OrgStructure apos ON ap8.id = apos.id
     LEFT JOIN ActionProperty_String aps ON aps.id = ap9.id
@@ -78,7 +78,7 @@ SELECT
     LEFT JOIN Person p4 ON apr5.value = p4.id AND p4.deleted = 0
     LEFT JOIN OrgStructure_HospitalBed oshb ON aphb.value = oshb.id
     LEFT JOIN OrgStructure os1 ON apos.value = os1.id
-    WHERE e.eventType_id = 84 and a.id in (83815) GROUP by a.id ORDER BY nomeroperacionn, smena;
+    WHERE e.eventType_id = 84 and a.id in (85026) GROUP by a.id ORDER BY nomeroperacionn, smena;
 
 select * from Action where id = 83816
 
