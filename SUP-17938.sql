15900ee27a999fc8ea4306fc7de069f15e05670a
Lustik_ak <lustik_alex@nilc.spb.ru>
2021-06-23 17:40:03 +0300

    23-06-2021_17:40

--- SUP-17938.sql
+++ SUP-17938.sql
@@ -24,12 +24,14 @@ from rbUserRight where name regexp 'очеред';
 
 
 select e.id, et.*
-from Action a, Event e, EventType et where e.id = a.event_id and a.actionType_id = 11021 and a.deleted = 0 and e.deleted = 0 group by et.id, e.id;
+from Action a, Event e, EventType et where e.id = a.event_id and a.actionType_id = 11021 and a.deleted = 0 and e.deleted = 0 group by e.id order by e.createDatetime desc;
 
 
 select *
 from rbUserProfile_Right where userRight_id = 318;
 
+insert into s11.rbUserProfile_Right (id, createDatetime, createPerson_id, modifyDatetime, modifyPerson_id, master_id, userRight_id)
+values  (1905, '2018-12-13 17:55:25', 614, '2021-01-27 16:09:58', 614, 1, 318);
 
 select *
 from rbUserProfile where id in(10,22,24,25,1);
