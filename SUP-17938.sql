48ecd22ca0d73572f15f637e1027c20fe54b558a
Lustik_ak <lustik_alex@nilc.spb.ru>
2021-06-23 16:10:01 +0300

    23-06-2021_16:10

---
+++ SUP-17938.sql
@@ -0,0 +1,22 @@
+select *
+from rbJobType;
+
+
+
+select *
+from Job where jobType_id = 64 and date = '2021-06-24';
+
+
+select *
+from rbUserRight where name regexp 'очеред';
+
+
+select *
+from rbUserProfile_Right where userRight_id = 318;
+
+
+select *
+from rbUserProfile where id in(10,22,24,25,1);
+
+select *
+from rbUserProfile;
