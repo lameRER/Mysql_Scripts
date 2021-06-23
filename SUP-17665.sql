0b3a4a405474c85177cab0361218f431b65bcaed
Lustik_ak <lustik_alex@nilc.spb.ru>
2021-06-23 14:20:01 +0300

    23-06-2021_14:20

--- SUP-17665.sql
+++ SUP-17665.sql
@@ -1,6 +1,8 @@
 use s12;
 set @flatCode = 'received';
 set @ActionType = (select id from ActionType where flatCode = @flatCode and deleted = 0);
+
+
 set @ActionPropertyType = 'Доставлен';
 set @ActionPropertyTypeOld = (select id from ActionPropertyType where name = @ActionPropertyType and actionType_id = @ActionType and deleted = 0 and typeName = 'String');
 set @ActionPropertyTypeNew = (select id from ActionPropertyType where name = @ActionPropertyType and actionType_id = @ActionType and deleted = 0 and typeName = 'Reference');
@@ -37,13 +39,8 @@ join ActionPropertyType apt on apt.actionType_id = at2.id and apt.deleted =0 and
 set ap.type_id = @ActionPropertyTypeNew
 where ap.deleted= 0 and aps.id is not null;
 
-
-UPDATE ActionPropertyType SET idx = 5, penalty = 100, penaltyUserProfile = '29; 7; 19; 32; 31; 53; 36; 37; 57; 48; 63', visibleInDR = 1, userProfile_id = null, userProfileBehaviour = 0 WHERE id = 38920;
-UPDATE ActionPropertyType SET idx = 20, penalty = 0, penaltyUserProfile = '7;19', visibleInDR = 0, userProfile_id = 1, userProfileBehaviour = 1 WHERE id = 35423;
-
-
-
-
+UPDATE ActionPropertyType SET idx = 5, penalty = 100, penaltyUserProfile = '29; 7; 19; 32; 31; 53; 36; 37; 57; 48; 63', visibleInDR = 1, userProfile_id = null, userProfileBehaviour = 0 WHERE id = @ActionPropertyTypeNew;
+UPDATE ActionPropertyType SET idx = 20, penalty = 0, penaltyUserProfile = '7;19', visibleInDR = 0, userProfile_id = 1, userProfileBehaviour = 1 WHERE id = @ActionPropertyTypeOld;
 
 
 set @ActionPropertyTypeOld = (select id from ActionPropertyType where name = 'Кем доставлен' and actionType_id = @ActionType and deleted = 0 and typeName = 'String');
@@ -72,20 +69,20 @@ join ActionType at2 on at2.id = a.actionType_id and at2.deleted = 0 and at2.flat
 join ActionPropertyType apt on apt.actionType_id = at2.id and apt.deleted =0 and ap.type_id = apt.id and apt.id = @ActionPropertyTypeOld
 where ap.deleted= 0 and aps.id is not null;
 
-UPDATE ActionPropertyType SET idx = 2, visibleInDR = 1, userProfile_id = null, userProfileBehaviour = 0 WHERE id = 38922;
-UPDATE ActionPropertyType SET idx = 21, visibleInDR = 0, userProfile_id = 1, userProfileBehaviour = 1 WHERE id = 35418;
+update
+# select ap.* from
+    ActionProperty ap
+left Join ActionProperty_String aps using(id)
+join Action a on ap.action_id = a.id and a.deleted = 0
+join ActionType at2 on at2.id = a.actionType_id and at2.deleted = 0 and at2.flatCode = @flatCode
+join ActionPropertyType apt on apt.actionType_id = at2.id and apt.deleted =0 and ap.type_id = apt.id and apt.id = @ActionPropertyTypeOld
+set ap.type_id = @ActionPropertyTypeNew
+where ap.deleted= 0 and aps.id is not null;
 
 
-
-
+UPDATE ActionPropertyType SET idx = 2, visibleInDR = 1, userProfile_id = null, userProfileBehaviour = 0 WHERE id = @ActionPropertyTypeNew;
+UPDATE ActionPropertyType SET idx = 21, visibleInDR = 0, userProfile_id = 1, userProfileBehaviour = 1 WHERE id = @ActionPropertyTypeOld;
 
 
 select *
-from ActionProperty_String_copy_07_06_21;
-
-
-select *
-from ActionProperty_String_copy_23_06_21;
-
-
-
+from ActionPropertyType where valueDomain in ('netricaTypeFromDiseaseStart', 'netricaTransportIntern', 'netricaHospChannel', 'netricaPatientConditionOnAdmission', 'netricaIntoxicationType', 'rbTransf') and actionType_id = 15084;
