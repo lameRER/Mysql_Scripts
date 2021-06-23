e61d71bb3ff63dd06482c5614047928c68542613
Lustik_ak <lustik_alex@nilc.spb.ru>
2021-06-23 16:40:02 +0300

    23-06-2021_16:40

--- SUP-17938.sql
+++ SUP-17938.sql
@@ -4,6 +4,18 @@ from rbJobType;
 
 
 select *
+from ActionPropertyType where typeName = 'JobTicket' and valueDomain = '15' ;
+
+
+select *
+from ActionType where id = 11021;
+
+
+
+select *
+from ActionType where group_id = 11018;
+
+select *
 from Job where jobType_id = 64 and date = '2021-06-24';
 
 
@@ -23,4 +35,6 @@ from rbUserProfile;
 
 
 select *
-from Person where lastName regexp 'Юрьевна';
+from Person where lastName regexp 'Юрьева';
+
+select md5('')
