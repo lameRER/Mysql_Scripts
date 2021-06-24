b9969c3389f8e446e8525a312280ed368a6bfd26
Lustik_ak <lustik_alex@nilc.spb.ru>
2021-06-24 10:30:01 +0300

    24-06-2021_10:30

--- GNC-73.sql
+++ GNC-73.sql
@@ -18,6 +18,51 @@ VALUES (34408, 'Date', 'Date2', 'Введите дату окончания пе
 
 
 
+SELECT
+  a.createDatetime as `Дата установления диагноза`,
+  e.client_id as `№ карты`,
+  CONCAT_WS(' ', c.lastName, c.firstName, c.patrName) as `ФИО`,
+  IF(c.sex = 1, "Мужской", "Женский") as `Пол`,
+  TIMESTAMPDIFF(year, c.birthDate, e.setDate) as `Возраст`,
+  CONCAT(r.SOCR,'. ', r.NAME) as `Регион`,
+  GROUP_CONCAT(DISTINCT CONCAT(
+  CASE WHEN d.MKB IS NOT NULL THEN d.MKB END,
+  CASE WHEN LENGTH(m.DiagName) > 0 THEN CONCAT(' - ', m.DiagName) ELSE '' END,
+  CASE WHEN LENGTH(TRIM(ReportStripTags(d.diagnosis_description))) > 0 THEN CONCAT(' - ', ReportStripTags(d.diagnosis_description)) ELSE '' END
+  )
+  SEPARATOR '; ') as `Диагноз по МКБ и описание`,
+  CONCAT(p.lastName, ' ', LEFT(p.firstName, 1), '.', LEFT(p.patrName, 1), '.') as `Врач`
+FROM Event e
+	INNER JOIN Action a
+  	ON a.event_id = e.id
+	INNER JOIN ActionType at
+  	ON a.actionType_id = at.id
+  INNER JOIN Client c
+    ON c.id = e.client_id
+  LEFT OUTER JOIN ReportClientRegion r
+    ON r.client_id = e.client_id
+    AND r.client_address_type = 0
+  INNER JOIN Person p
+    ON p.id = a.person_id
+  INNER JOIN Action_Diagnosis ad
+    ON ad.action_id = a.id
+    AND ad.diagnosisType_id = 2 /* клинический */
+    AND ad.diagnosisKind_id = 4 /* основной */
+    AND ad.deleted = 0
+  INNER JOIN Diagnostic d
+    ON d.diagnosis_id = ad.diagnosis_id
+    AND d.deleted = 0
+  INNER JOIN MKB m
+    ON m.DiagID = d.MKB
+    AND m.deleted = 0
+WHERE a.begDate BETWEEN :Date1 AND :Date2 + INTERVAL 1 DAY - INTERVAL 1 SECOND
+    AND e.deleted = 0
+    AND e.client_id != 18
+    AND at.class=0
+    AND m.id = :Diagnoses
+GROUP BY c.id, p.id
+ORDER BY a.createDatetime
+
 
 
 
