78e9a835ce092f23600e2267a945a7d0b7f36e96
Lustik_ak <lustik_alex@nilc.spb.ru>
2021-07-01 16:00:01 +0300

    01-07-2021_16:00

--- SUP-17665.sql
+++ SUP-17665.sql
@@ -121,8 +121,8 @@ join ActionPropertyType apt on apt.actionType_id = at2.id and apt.deleted =0 and
 where ap.deleted= 0 and aps.id is not null) as tmp
 where not exists(select * from ActionProperty where action_id = tmp.action_id and type_id = tmp.type_id and deleted = tmp.deleted);
 
-# insert into ActionProperty_Reference(id, `index`, value)
-select ap1.id, 0, regexp_replace(aps.value, '\\D', ''), aps.value
+insert into ActionProperty_Reference(id, `index`, value)
+select ap1.id, 0, regexp_replace(aps.value, '\\D', '') as `value`
 from ActionProperty ap
 join ActionProperty_String aps on aps.id = ap.id
 join Action a on ap.action_id = a.id and a.deleted = 0
