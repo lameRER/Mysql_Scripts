59978cd6c74a304dcf18f00984d80ae9a8b9cef8
Lustik_ak <lustik_alex@nilc.spb.ru>
2021-07-01 17:15:01 +0300

    01-07-2021_17:15

--- SUP-17665.sql
+++ SUP-17665.sql
@@ -122,7 +122,7 @@ where ap.deleted= 0 and aps.id is not null) as tmp
 where not exists(select * from ActionProperty where action_id = tmp.action_id and type_id = tmp.type_id and deleted = tmp.deleted);
 
 # insert into ActionProperty_Integer(id, `index`, value)
-select ap1.id, 0, regexp_replace(if(aps.value is null, 0, aps.value), '\\D', '') as `value`
+select ap1.id, 0, regexp_replace(if(aps.value is null, 0, aps.value), '\\D', '') as `value`, aps.value
 from ActionProperty ap
 join ActionProperty_String aps on aps.id = ap.id
 join Action a on ap.action_id = a.id and a.deleted = 0
