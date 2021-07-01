b73c079b670a7cb88b6be3f5ba19696ff95c7694
Lustik_ak <lustik_alex@nilc.spb.ru>
2021-07-01 12:00:01 +0300

    01-07-2021_12:00

--- SUP-17665.sql
+++ SUP-17665.sql
@@ -37,20 +37,21 @@ join ActionType at2 on at2.id = a.actionType_id and at2.deleted = 0 and at2.flat
 join ActionPropertyType apt on apt.actionType_id = at2.id and apt.deleted =0 and ap.type_id = apt.id and apt.id = @ActionPropertyTypeOld
 where ap.deleted= 0 and aps.id is not null;
 
-# insert into ActionProperty_Reference(id, `index`, value)
-select count(ap1.id) ,ap1.id, 0, case
+insert into ActionProperty_Reference(id, `index`, value)
+select ap1.id, 0, case
            when aps.value regexp '^в первые 6часов|^1 час|^2 часа|^3 часа|^4|^4 часа|^5 часов|^6 часов|^< font style="vertical-align: inherit;">< font style="vertical-align: inherit;">1 час< /font>< /font>|^< font style="vertical-align: inherit;">< font style="vertical-align: inherit;">4 часа< /font>< /font>' then 4
            when  aps.value regexp '^в течении 7-24 часов|^7-12 часов|^7-24 часов|^12-24 часов' then 5
            when aps.value regexp '^менее 2 суток|^более 24-х часов|^более 7 суток|^менее 3 суток|^менее 4 суток|^менее 5 суток|^менее 6 суток|^менее 7 суток|^позднее 24-х часов' then 6 end
 from ActionProperty ap
-left join ActionProperty_String aps on aps.id = ap.id
+join ActionProperty_String aps on aps.id = ap.id
 join Action a on ap.action_id = a.id and a.deleted = 0
-join ActionProperty ap1 on ap1.action_id = a.id
-left join ActionProperty_Reference apr on apr.id = ap1.id
+join ActionProperty ap1 on ap1.action_id = a.id and ap1.deleted = 0 and ap1.type_id = @ActionPropertyTypeNew
+join ActionProperty_Reference apr on apr.id = ap1.id
 join ActionType at2 on at2.id = a.actionType_id and at2.deleted = 0 and at2.flatCode = @flatCode
-join ActionPropertyType apt on apt.actionType_id = at2.id and apt.deleted =0 and ap.type_id = apt.id and apt.id = @ActionPropertyTypeOld
-join ActionPropertyType apt1 on apt1.actionType_id = at2.id and apt1.deleted =0 and ap1.type_id = apt1.id and apt1.id = @ActionPropertyTypeNew
-where ap.deleted= 0 group by ap1.id having count(ap1.id)>1;
+where ap.deleted= 0 and ap.type_id = @ActionPropertyTypeOld;
+
+select *
+from ActionProperty_Reference where id =201956380;
 
 # insert into ActionProperty_Reference(id, `index`, value)
 # select *
