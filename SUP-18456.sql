4926d1b71afc93a74cc8a07c7e538a02bcf0c293
Lustik_ak <lustik_alex@nilc.spb.ru>
2021-07-05 17:15:01 +0300

    05-07-2021_17:15

---
+++ SUP-18456.sql
@@ -0,0 +1,18 @@
+select *
+from ActionType where name regexp 'Осмотр дежурного х';
+
+
+select * from DestinationTree_ActionType where actionType_id = 22165
+
+
+select *
+from DestinationTree where id =10029;
+
+
+
+select *
+from OrgStructure_ActionType where actionType_id = 22165;
+
+
+select *
+from OrgStructure where id = 107;
