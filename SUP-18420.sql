12cbfff50ead00915a25bd1e686a1e94a8d95584
Lustik_ak <lustik_alex@nilc.spb.ru>
2021-07-05 12:15:01 +0300

    05-07-2021_12:15

--- SUP-18420.sql
+++ SUP-18420.sql
@@ -2,4 +2,13 @@ select *
 from rbUserRight where name regexp 'шаблоны';
 
 
-select * from rbUserProfile_Right where userRight_id = 95
+select * from rbUserProfile_Right, rbUserProfile where rbUserProfile_Right.userRight_id = 1 and rbUserProfile.id = rbUserProfile_Right.master_id
+
+
+select *
+from Person where login = 'виста';
+
+
+
+select *
+from rbUserProfile_Right, rbUserRight where rbUserProfile_Right.master_id = 24 and rbUserRight.id = rbUserProfile_Right.userRight_id;
