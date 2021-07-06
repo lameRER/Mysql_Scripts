e877c085e50c7b3d37deaa78f5fd644a525ff520
Lustik_ak <lustik_alex@nilc.spb.ru>
2021-07-06 10:45:02 +0300

    06-07-2021_10:45

--- SUP-18420.sql
+++ SUP-18420.sql
@@ -8,6 +8,15 @@ from s12.rbUserProfile_Right where userRight_id = 1;
 insert into s12.rbUserProfile_Right (id, createDatetime, createPerson_id, modifyDatetime, modifyPerson_id, master_id, userRight_id)
 values  (1, '2017-06-16 15:02:59', 1069, '2021-06-11 16:08:57', 200, 1, 1);
 
+
+select *
+from rbUserProfile where id = 24;
+
+
+select *
+from Person where userProfile_id = 24;
+
+
 select *
 from Person where login = 'виста';
 
