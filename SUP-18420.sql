81437dddff5f7d3ff5a2ff85090d6b2fa50c2c7a
Lustik_ak <lustik_alex@nilc.spb.ru>
2021-07-06 11:15:01 +0300

    06-07-2021_11:15

--- SUP-18420.sql
+++ SUP-18420.sql
@@ -9,6 +9,15 @@ insert into rbUserProfile_Right (id, createDatetime, createPerson_id, modifyDate
 values  (1, '2017-06-16 15:02:59', 1069, '2021-06-11 16:08:57', 200, 1, 1);
 
 
+# select *
+delete
+from rbUserProfile_Right
+where id =3393;
+
+insert into rbUserProfile_Right (id, createDatetime, createPerson_id, modifyDatetime, modifyPerson_id, master_id, userRight_id)
+values  (3393, '2018-04-13 17:32:18', 356, '2021-06-11 16:04:09', 200, 24, 1);
+
+
 select *
 from rbUserProfile where id = 24;
 
@@ -19,6 +28,12 @@ select * from
 #      set password = 'd68a18275455ae3eaa2c291eebb46e6d'
      where userProfile_id = 24 and retireDate is null
 ;
+
+
+
+select rbUserProfile.name
+from rbUserProfile;
+
 
 d68a18275455ae3eaa2c291eebb46e6d
 select *from Person where login = 'виста';
