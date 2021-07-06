68eb349359b6e56ff6407cd4850d5ee65ed87849
Lustik_ak <lustik_alex@nilc.spb.ru>
2021-07-06 16:30:01 +0300

    06-07-2021_16:30

--- SUP-18483.sql
+++ SUP-18483.sql
@@ -13,16 +13,16 @@ from logger.;
 select *
 from logger.N3LabOrderLog l
 left join logger.N3LabOrderSyncLog ls on ls.order_id = l.id
-where l.takenTissueJournal_id = 104445
+where l.takenTissueJournal_id = 104455
 
 
 
 select *
-from s11.TakenTissueJournal where externalId regexp '210021411';
+from s11.TakenTissueJournal where externalId regexp '210021395';
 
 
 select *
-from s11.TakenTissueJournal where id = 104445;
+from s11.TakenTissueJournal where id = 104455;
 
 
 select *
