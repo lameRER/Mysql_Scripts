c855d91dc043776d40f63d3bfc461d0db4178eed
Lustik_ak <lustik_alex@nilc.spb.ru>
2021-07-07 11:15:01 +0300

    07-07-2021_11:15

--- SUP_Petrova-1934.sql
+++ SUP_Petrova-1934.sql
@@ -1,2 +1,10 @@
-select *
+select id, `default`
 from rbPrintTemplate where id = 1132;
+
+
+select orgStructure_id
+from Person where login = 'табагуатт';
+
+
+select chief_id
+from OrgStructure where id = 171;
