ca0ee327e0d3c68f51ade6199a09e8e73af4a01a
Lustik_ak <lustik_alex@nilc.spb.ru>
2021-07-12 18:00:01 +0300

    12-07-2021_18:00

--- test2.sql
+++ test2.sql
@@ -1,6 +1,6 @@
 SELECT ClassID, someSubQueryTable.id
 FROM (Select id, getMKBClassID(DiagID) as ClassID from MKB_Tree as MKB2) AS someSubQueryTable
-WHERE (someSubQueryTable.id = 21788)
+WHERE (someSubQueryTable.id = 21788) and ClassID = 'Error'
 ORDER BY someSubQueryTable.id DESC;
 
 
