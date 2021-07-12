c426438649d8e1b28976c90e5af7e6c8f449eb4d
Lustik_ak <lustik_alex@nilc.spb.ru>
2021-07-12 10:00:02 +0300

    12-07-2021_10:00

--- GNC-78.sql
+++ GNC-78.sql
@@ -1,11 +1,13 @@
 select
        c.id `Код пациента`,
-       e.setDate `Дата посещения`
+       e.setDate `Дата посещения`,
+       os.name `Отделение`
 from Event e
 join Client c on e.client_id = c.id and c.deleted = 0
 join EventType et on et.id = e.eventType_id and et.deleted = 0 and et.purpose_id = 2
 join Action a on a.event_id = e.id and a.deleted = 0
-join ActionType at on at.id = a.actionType_id and at.deleted = 0 and at.flatCode = 'moving'
+join Person p on p.id = a.person_id and p.deleted = 0
+join OrgStructure os on os.id = p.orgStructure_id
 # join rbFinance rf on et.finance_id = rf.id
 
 # join Service s on s.action_id = a.id
@@ -19,9 +21,16 @@ where e.setDate >= '2018-01-01' and e.setDate <= '2021-06-30'
 select * from ActionType where flatCode = 'moving' and deleted = 0
 
 
-select *
-from EventType where deleted = 0 and purpose_id = 2;
 
+                           and purpose_id = 2;
+
+
+select *
+from Event where externalId regexp '61678';
+
+
+select *
+from EventType where id = 109;
 
 select *
 from Diagnostic;
