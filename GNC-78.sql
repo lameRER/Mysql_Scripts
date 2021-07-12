57fc875913f156b944024026f218c63652ba9ade
Lustik_ak <lustik_alex@nilc.spb.ru>
2021-07-12 11:30:01 +0300

    12-07-2021_11:30

--- GNC-78.sql
+++ GNC-78.sql
@@ -4,7 +4,8 @@ select
        os.name `Отделение`,
        rf.name `Источник финансирования`,
        concat_ws(' ', p.lastName, p.firstName, p.patrName) `Врач`,
-       d1.MKB `Основной диагноз`
+       d1.MKB `Основной диагноз`,
+       s.sum `Стоимость`
 from Event e
 join Client c on e.client_id = c.id and c.deleted = 0
 join EventType et on et.id = e.eventType_id and et.deleted = 0 and et.purpose_id = 2
@@ -13,12 +14,15 @@ left join OrgStructure os on os.id = e.orgStructure_id
 left join rbFinance rf on et.finance_id = rf.id
 left join Diagnosis d on d.client_id = c.id and d.deleted = 0
 left join Diagnostic d1 on d1.event_id = e.id and d.id = d1.diagnosis_id and d1.deleted = 0
-# left join Action a on a.event_id = e.id and a.deleted = 0
-# join Service s on s.action_id = a.id
-# join PriceListItem pli on pli.id = s.PriceListItem_id
+left join Action a on a.event_id = e.id and a.deleted = 0
+left join Service s on s.action_id = a.id and s.event_id = e.id and s.deleted = 0
+join PriceListItem pli on pli.id = s.PriceListItem_id
 
 where e.setDate >= '2018-01-01' and e.setDate <= '2021-06-30' group by e.id limit 10
 
+
+select *
+from Service;
 
 
 select d.*
