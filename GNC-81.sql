fbe6cccea07cc8a0c00df4a5a615d43bb401e0c6
Lustik_ak <lustik_alex@nilc.spb.ru>
2021-07-13 10:15:01 +0300

    13-07-2021_10:15

---
+++ GNC-81.sql
@@ -0,0 +1,6 @@
+select *
+from rbPrintTemplate where name = 'Сводная 007 за период ПО ВСЕМ ОТДЕЛЕНИЯМ';
+
+
+select *
+from rbSpecialVariablesPreferences where name = 'SpecialVar_form007day_s_vl_2';
