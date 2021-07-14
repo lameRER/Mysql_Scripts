4b1fd0b51f15449318570437566ae39c63e619bb
Lustik_ak <lustik_alex@nilc.spb.ru>
2021-07-14 10:15:02 +0300

    14-07-2021_10:15

--- SUP-18651.sql
+++ SUP-18651.sql
@@ -18,8 +18,67 @@ FROM
   INFORMATION_SCHEMA.KEY_COLUMN_USAGE
 WHERE
   REFERENCED_TABLE_SCHEMA = 's11' AND
-  REFERENCED_TABLE_NAME = 'ActionType' AND
+  REFERENCED_TABLE_NAME = 'Organisation' AND
   REFERENCED_COLUMN_NAME = 'id';
+
+set @orgOld = 3320;
+set @orgNew = 24726;
+
+
+select *
+from ClientPolicy;
+
+select @orgOld
+
+select * from Action where org_id = @orgOld;
+
+select * from ActionProperty_Organisation where value = @orgOld;
+
+select * from ActionType where defaultOrg_id = @orgOld;
+
+select * from Contract_Contingent where insurer_id = @orgOld;
+
+select * from Contract_Tariff where attachLPU_id = @orgOld;
+
+select * from Event where outgoingOrg_id = @orgOld;
+
+select * from Event where relegateOrg_id = @orgOld;
+
+select * from EventType_Action where defaultOrg_id = @orgOld;
+
+select * from Event_LocalContract where org_id = @orgOld;
+
+select * from Event_OutgoingReferral where org_id = @orgOld;
+
+select * from ForeignHospitalization where org_id = @orgOld;
+
+select * from Organisation_PolicySerial where organisation_id = @orgOld;
+
+select * from PaymentScheme where org_id = @orgOld;
+
+select * from Person where org_id = @orgOld;
+
+select * from Person_Education where org_id = @orgOld;
+
+select * from PoliclinicReferrals where organisation_id = @orgOld;
+
+select * from Referral where relegateOrgTo_id = @orgOld;
+
+select * from Referral where relegateOrg_id = @orgOld;
+
+select * from ZNOInfo where referredTo = @orgOld;
+
+select * from ZNOInfo where referredFrom = @orgOld;
+
+select * from rbAssignedContracts where org_id = @orgOld;
+
+
+select *
+from ActionType where defaultOrg_id = @orgOld;
+
+
+
+
 
 
 SELECT
