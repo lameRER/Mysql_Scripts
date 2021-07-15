9c6dcfee7f4183e596d96d59dfda9697934acedf
Lustik_ak <lustik_alex@nilc.spb.ru>
2021-07-15 15:15:01 +0300

    15-07-2021_15:15

--- GNC-82.sql
+++ GNC-82.sql
@@ -19,24 +19,36 @@ from Action where actionType_id = 5487 and createDatetime >= '2020-01-01' and cr
 
 
 select
-       e.id,
-       e.externalId,
-       c.id,
        concat_ws(' ', c.lastName, c.firstName, c.patrName),
-       a.createDatetime
-
-
-
+       a.createDatetime,
+        group_concat(concat_ws(': ', apt.name, aps.value) separator '\n')
 from ActionProperty ap
+left join ActionProperty_String aps on aps.id  = ap.id
 join Action a on ap.action_id = a.id and ap.deleted = 0
 join Event e on e.id = a.event_id and a.deleted = 0
 join Client c on c.id = e.client_id and c.deleted = 0
 join ActionType at on at.id = a.actionType_id and at.deleted = 0 and at.id = 5487
-join ActionPropertyType apt on apt.id = ap.type_id and apt.deleted =0 and at.id = apt.actionType_id
+join ActionPropertyType apt on apt.id = ap.type_id and apt.deleted =0 and at.id = apt.actionType_id group by a.id
+
+
+
+
+select
+       concat_ws(' ', c.lastName, c.firstName, c.patrName),
+       a.createDatetime,
+        group_concat(concat_ws(': ', apt.name, aps.value) separator '\n')
+from ActionProperty ap
+left join ActionProperty_String aps on aps.id  = ap.id
+join Action a on ap.action_id = a.id and ap.deleted = 0 and  a.createDatetime >= '2020-01-01' and a.createDatetime <= '2021-07-02'
+join Event e on e.id = a.event_id and a.deleted = 0
+join Client c on c.id = e.client_id and c.deleted = 0
+join ActionType at on at.id = a.actionType_id and at.deleted = 0 and at.id = 8443
+join ActionPropertyType apt on apt.id = ap.type_id and apt.deleted =0 and at.id = apt.actionType_id group by a.id
 
 
 select *
-from ActionPropertyType where actionType_id = 5487;
+from ActionPropertyType where actionType_id = 8443
+;
 
 
 select *
---
+++ Script.sql
@@ -0,0 +1,12 @@
+
+select
+       concat_ws(' ', c.lastName, c.firstName, c.patrName),
+       a.createDatetime,
+        group_concat(concat_ws(': ', apt.name, aps.value separator '\n'))
+from ActionProperty ap
+left join ActionProperty_String aps on aps.id  = ap.id
+join Action a on ap.action_id = a.id and ap.deleted = 0
+join Event e on e.id = a.event_id and a.deleted = 0
+join Client c on c.id = e.client_id and c.deleted = 0
+join ActionType at on at.id = a.actionType_id and at.deleted = 0 and at.id = 5487
+join ActionPropertyType apt on apt.id = ap.type_id and apt.deleted =0 and at.id = apt.actionType_id group by a.id
