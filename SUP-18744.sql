8f4d7e9c37c030b5499add55b37c0c96abf471bd
Lustik_ak <lustik_alex@nilc.spb.ru>
2021-07-16 09:45:01 +0300

    16-07-2021_09:45

--- SUP-18744.sql
+++ SUP-18744.sql
@@ -2,13 +2,24 @@ select regexp_replace(rbPrintTemplate.`default`, '(setLeftMargin\\().+?(\\))', '
        rbPrintTemplate.`default`,
        rbPrintTemplate.*
 from rbPrintTemplate where `default` regexp 'setLeftMargin' and deleted = 0
+;
 
 
-select regexp_replace(rbPrintTemplate.`default`, '\\d+', '123456789'),
-       rbPrintTemplate.`default`,
-       rbPrintTemplate.*
-from rbPrintTemplate where `default` regexp 'setLeftMargin' and deleted = 0
+update rbPrintTemplate
+set `default` = regexp_replace(rbPrintTemplate.`default`, '(setLeftMargin\\().+?(\\))', '\\120\\2')
+where `default` regexp 'setLeftMargin' and deleted = 0
+
+
+
+
+
+select regexp_replace(rbPrintTemplate.`default`, '(<head>)','\\1\n{: setLeftMargin(20) }'), rbPrintTemplate.`default`, rbPrintTemplate.*
+from rbPrintTemplate where `default` not regexp 'setLeftMargin'  and `default` regexp '<head>' and deleted = 0;
 
 
 select *
-from rbPrintTemplate where `default` not regexp 'setLeftMargin' and deleted = 0;
+from Organisation where id = 6821
+
+
+select *
+from Person where org_id is not null group by org_id;
