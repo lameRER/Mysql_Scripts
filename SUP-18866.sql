71485ee644ef582373a6b646947c11d7e483d820
Lustik_ak <lustik_alex@nilc.spb.ru>
2021-07-20 12:30:01 +0300

    20-07-2021_12:30

--- SUP-18866.sql
+++ SUP-18866.sql
@@ -1,2 +1,28 @@
 select *
-from ;
+from rbPrintTemplate
+where context =
+(select context
+from ActionType where name = 'Первичный осмотр дежурного врача' and deleted =0 and code = '145-17746F')
+
+
+
+select *
+from rbPrintTemplate where context =
+(select context
+from ActionType where name = 'Назначение НСПВ');
+
+
+select *
+from ActionPropertyType where actionType_id =
+(select id
+from ActionType where name = 'Назначение НСПВ')
+
+
+
+select *
+from rbPrintTemplate where context in
+(select context
+from ActionType where id in
+(select actionType_id
+from ActionPropertyType where typeName = 'Action' and deleted=0 ) and deleted =0)
+;
