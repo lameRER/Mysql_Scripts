1241f8202d3c571c04bae4677dd07dc9c376c062
Lustik_ak <lustik_alex@nilc.spb.ru>
2021-07-20 09:45:01 +0300

    20-07-2021_09:45

--- SUP-18250.sql
+++ SUP-18250.sql
@@ -1,5 +1,5 @@
 select *
-from s12.rbPrintTemplate where name regexp 'Шил';
+from s12.rbPrintTemplate where rbPrintTemplate.context regexp 'clientbanner';
 
 select *
 from information_schema.PROCESSLIST where COMMAND not in ('sleep') order by TIME desc;
