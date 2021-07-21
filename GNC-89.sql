254d8b41dbeb6a6a82dbc485924a14d53ea4ac22
Lustik_ak <lustik_alex@nilc.spb.ru>
2021-07-21 09:30:01 +0300

    21-07-2021_09:30

---
+++ GNC-89.sql
@@ -0,0 +1,11 @@
+
+insert into rbSpeciality(code, name)
+select
+       '' code,
+       'нефролог/гематолог' name
+from rbSpeciality where name = 'нефролог';
+
+
+select *
+from rbSpeciality;
+
--- SUP-18250.sql
+++ SUP-18250.sql
@@ -2,7 +2,12 @@ select *
 from s12.rbPrintTemplate where rbPrintTemplate.context regexp 'clientbanner';
 
 select *
-from information_schema.PROCESSLIST where COMMAND not in ('sleep') order by TIME desc;
+from information_schema.PROCESSLIST where COMMAND not in ('sleep') and DB= 's12' order by TIME desc;
+
+
+
+
+SELECT id FROM Action WHERE Action.`event_id` = 3921176  ORDER BY id
 
 
 
