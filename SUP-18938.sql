eabc96e4c3c1751822669a8f6fa288dfbda3034b
Lustik_ak <lustik_alex@nilc.spb.ru>
2021-07-23 11:45:01 +0300

    23-07-2021_11:45

--- SUP-18938.sql
+++ SUP-18938.sql
@@ -12,32 +12,47 @@ set @acOld = 43232;
 set @acNew = 43419;
 
 
+select ap.* from ActionProperty ap
+left JOIN ActionProperty_Job_Ticket apjt using(id)
+ JOIN Job_Ticket jt ON apjt.value = jt.id and jt.datetime >= '2021-07-23 13:00:00'
+join Action a on a.id = ap.action_id and a.deleted =0 and a.status = 1
+join ActionType at1 on at1.id = a.actionType_id and at1.deleted = 0 and at1.id = @acOld
+left join ActionPropertyType apt on apt.actionType_id = at1.id and apt.id = ap.type_id and apt.deleted = 0
+left join ActionType at2 on at2.id = @acNew
+left join ActionPropertyType apt1 on apt1.actionType_id = at2.id and apt1.deleted = 0 and apt1.name = apt.name and apt1.typeName = apt.typeName and apt1.idx = apt.idx;
 
-select ap.id,
-       ap.createDatetime,
-       ap.createPerson_id,
-       ap.modifyDatetime,
-       ap.modifyPerson_id,
-       ap.deleted,
-       apt1.id,
-       action_id,
-       type_id,
-       ap.unit_id,
-       ap.norm,
-       isAssigned,
-       evaluation,
-       isAutoFillCancelled
+
+
+
+PREPARE stmt_name FROM @sel;
+EXECUTE stmt_name;
+
+
+
+select *
 from ActionProperty ap
-JOIN ActionProperty_Job_Ticket apjt using(id)
-JOIN Job_Ticket jt ON apjt.value = jt.id and jt.datetime >= '2021-07-23 11:30:00'
+left JOIN ActionProperty_Job_Ticket apjt using(id)
+ left JOIN Job_Ticket jt ON apjt.value = jt.id and jt.datetime >= '2021-07-23 11:30:00'
 join Action a on a.id = ap.action_id and a.deleted =0 and a.status = 1
 join ActionType at1 on at1.id = a.actionType_id and at1.deleted = 0 and at1.id = @acOld
 join ActionPropertyType apt on apt.actionType_id = at1.id and apt.id = ap.type_id and apt.deleted = 0
 left join ActionType at2 on at2.id = @acNew
-left join ActionPropertyType apt1 on apt1.actionType_id = at2.id and apt1.deleted = 0
+left join ActionPropertyType apt1 on apt1.actionType_id = at2.id and apt1.deleted = 0 and apt1.name = apt.name and apt1.typeName = apt.typeName and apt1.idx = apt.idx
 
 
 
+select *
+from ActionPropertyType where id in(14568,13624,14569,13625,14570,13626,14571,13627,14573,13629,14574,13630,14575,13631,14577,13633,14578,13634,14579,13635,14580,13636,14581,13637,14585,13641,14586,13642,14587,13643,14588,13644,14589,13645,14590,13646,14591,13647,14592,13648);
+
+
+
+
+select *
+from information_schema.PROCESSLIST where COMMAND not in('Sleep') and HOST regexp '10.145.134.254';
+
+
+select *
+from ActionPropertyType where id in();
 
 select * from ActionPropertyType where actionType_id = (select id from ActionType where id = @acOld) and deleted = 0;
 
