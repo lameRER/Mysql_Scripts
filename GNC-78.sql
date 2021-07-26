5469162dbc9e6e1dac384f4f7eeed1b9a562c145
Lustik_ak <lustik_alex@nilc.spb.ru>
2021-07-26 13:15:01 +0300

    26-07-2021_13:15

--- GNC-78.sql
+++ GNC-78.sql
@@ -9,15 +9,15 @@ select
        SUM(s.sum) `Общая стоимость`
 from Event e
 join Client c on e.client_id = c.id and c.deleted = 0
-join EventType et on et.id = e.eventType_id and et.deleted = 0 and et.purpose_id = 2
-left join Person p on p.id = e.execPerson_id and p.deleted = 0
-left join OrgStructure os on os.id = e.orgStructure_id and os.deleted = 0
-left join rbFinance rf on et.finance_id = rf.id and rf.deleted = 0
+join EventType et on et.id = e.eventType_id and et.purpose_id = 2
+left join Person p on p.id = e.execPerson_id
+left join OrgStructure os on os.id = e.orgStructure_id
+left join rbFinance rf on et.finance_id = rf.id
 left join Event_Diagnosis ed on ed.event_id = e.id and ed.diagnosisType_id = 2 and ed.diagnosisKind_id = 4 and ed.deleted = 0
 left join Diagnosis d on d.id = ed.diagnosis_id and c.id= d.client_id and d.deleted = 0
 left join Diagnostic d1 on d1.diagnosis_id = d.id and d1.deleted = 0
 join Action a on a.event_id = e.id and a.deleted = 0 and a.status = 2
-join ActionType at on at.id = a.ActionType_id and at.deleted = 0
+join ActionType at on at.id = a.ActionType_id
 left join Service s on s.action_id = a.id and s.event_id = e.id and s.deleted = 0
 left join PriceListItem pli on pli.id = s.PriceListItem_id
 where e.setDate >= '2018-01-01' and e.setDate <= '2021-06-30' and e.deleted = 0
@@ -86,19 +86,19 @@ select
        s.sum `Стоимость`
 from Event e
 join Client c on e.client_id = c.id and c.deleted = 0
-join EventType et on et.id = e.eventType_id and et.deleted = 0 and et.purpose_id = 2
-left join rbFinance rf on et.finance_id = rf.id and rf.deleted = 0
+join EventType et on et.id = e.eventType_id and et.purpose_id = 2
+left join rbFinance rf on et.finance_id = rf.id
 left join Diagnosis d on d.client_id = c.id and d.deleted = 0 and d.diagnosisType_id = 2
 left join Diagnostic d1 on d1.event_id = e.id and d.id = d1.diagnosis_id and d1.deleted = 0
 join Action a on a.event_id = e.id and a.deleted = 0 and a.status = 2
-join ActionType at on at.id = a.ActionType_id and at.deleted = 0 and at.class = 2
-left join Person p on p.id = a.person_id and p.deleted = 0
-left join OrgStructure os on os.id = p.orgStructure_id and os.deleted = 0
-left join Service s on s.action_id = a.id and s.event_id = e.id and s.deleted = 0
+join ActionType at on at.id = a.ActionType_id and at.class = 2
+left join Person p on p.id = a.person_id
+left join OrgStructure os on os.id = p.orgStructure_id
+left join Service s on s.action_id = a.id and s.event_id = e.id
 left join PriceListItem pli on pli.id = s.PriceListItem_id
 where e.setDate >= '2018-01-01' and e.setDate <= '2021-06-30' and e.deleted = 0
 # and e.id = 3526258
-group by e.id, a.id order by c.id, a.createDatetime
+group by e.id, a.id order by c.id, a.createDatetime desc
 
 
 select
