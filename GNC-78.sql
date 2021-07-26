b3569eed3b7141436f59cde7aa6b6c66e7f7bd3f
Lustik_ak <lustik_alex@nilc.spb.ru>
2021-07-26 13:00:01 +0300

    26-07-2021_13:00

--- GNC-78.sql
+++ GNC-78.sql
@@ -102,8 +102,6 @@ group by e.id, a.id order by c.id, a.createDatetime
 
 
 select
-       e.id, e.externalId,
-       et.id,
        c.id `Код пациента`,
        at.name `Наименование манипуляции`,
        a.endDate `Дата манипуляции`,
@@ -113,25 +111,25 @@ select
        s.sum `Стоимость`
 from Event e
 join Client c on e.client_id = c.id and c.deleted = 0
-join EventType et on et.id = e.eventType_id and et.deleted = 0 and et.purpose_id = 2
-left join rbFinance rf on et.finance_id = rf.id and rf.deleted = 0
+join EventType et on et.id = e.eventType_id and et.purpose_id = 2
+left join rbFinance rf on et.finance_id = rf.id
 left join Diagnosis d on d.client_id = c.id and d.deleted = 0 and d.diagnosisType_id = 2
 left join Diagnostic d1 on d1.event_id = e.id and d.id = d1.diagnosis_id and d1.deleted = 0
 join Action a on a.event_id = e.id and a.deleted = 0 and a.status = 2
-join ActionType at on at.id = a.ActionType_id and at.deleted = 0 and at.class = 1 and at.context != 'action_lab'
-left join Person p on p.id = a.person_id and p.deleted = 0
-left join OrgStructure os on os.id = p.orgStructure_id and os.deleted = 0
-left join Service s on s.action_id = a.id and s.event_id = e.id and s.deleted = 0
+join ActionType at on at.id = a.ActionType_id and at.class = 1 and at.context != 'action_lab'
+left join Person p on p.id = a.person_id
+left join OrgStructure os on os.id = p.orgStructure_id
+left join Service s on s.action_id = a.id and s.event_id = e.id
 left join PriceListItem pli on pli.id = s.PriceListItem_id
 where e.setDate >= '2018-01-01' and e.setDate <= '2021-06-30' and e.deleted = 0
-and e.id = 20287049
+# and e.id = 20287049
 group by e.id, a.id order by c.id, a.createDatetime desc
 
 select *
 from rbFinance;
 
-select et.*
-from Event e, EventType et where e.id = 20287049 and et.id = e.eventType_id;
+select rf.*
+from Event e, EventType et, rbFinance rf where e.id = 20287049 and et.id = e.eventType_id and et.purpose_id = 2 and et.deleted = 0 and e.deleted = 0 and rf.id = et.finance_id;
 
 select *
 from ActionProperty ap
@@ -192,15 +190,15 @@ select
        s.sum `Стоимость`
 from Event e
 join Client c on e.client_id = c.id and c.deleted = 0
-join EventType et on et.id = e.eventType_id and et.deleted = 0 and et.purpose_id = 2
-left join rbFinance rf on et.finance_id = rf.id and rf.deleted = 0
+join EventType et on et.id = e.eventType_id and et.purpose_id = 2
+left join rbFinance rf on et.finance_id = rf.id
 left join Diagnosis d on d.client_id = c.id and d.deleted = 0 and d.diagnosisType_id = 2
 left join Diagnostic d1 on d1.event_id = e.id and d.id = d1.diagnosis_id and d1.deleted = 0
 join Action a on a.event_id = e.id and a.deleted = 0 and a.status = 2
-join ActionType at on at.id = a.ActionType_id and at.deleted = 0 and at.context = 'action_lab' and at.class =1
-left join Person p on p.id = a.person_id and p.deleted = 0
-left join OrgStructure os on os.id = p.orgStructure_id and os.deleted = 0
-left join Service s on s.action_id = a.id and s.event_id = e.id and s.deleted = 0
+join ActionType at on at.id = a.ActionType_id and at.context = 'action_lab' and at.class =1
+left join Person p on p.id = a.person_id
+left join OrgStructure os on os.id = p.orgStructure_id
+left join Service s on s.action_id = a.id and s.event_id = e.id
 left join PriceListItem pli on pli.id = s.PriceListItem_id
 where e.setDate >= '2018-01-01' and e.setDate <= '2021-06-30' and e.deleted = 0
 # and e.id = 3526258
