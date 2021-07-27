dfc4771a551d710c53b3666f17298dae232582a0
Lustik_ak <lustik_alex@nilc.spb.ru>
2021-07-27 18:00:01 +0300

    27-07-2021_18:00

---
+++ GNC-91.sql
@@ -0,0 +1,13 @@
+select *
+from rbPrintTemplate where name = 'Карта стационарного больного (ф. 003/у) страница 1' and context = 'f066';
+
+select EventType.requestType_id, EventType.*
+from EventType where id = 115;
+
+
+select *
+from rbRequestType;
+
+
+select *
+from EventType;
