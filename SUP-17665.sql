7aeb10175d095fa2d83e27292201fbcbf991d4a3
Lustik_ak <lustik_alex@nilc.spb.ru>
2021-07-28 14:00:13 +0300

    28-07-2021_14:00

--- SUP-17665.sql
+++ SUP-17665.sql
@@ -6,8 +6,8 @@ set @ActionPropertyType = 'Доставлен';
 set @ActionPropertyTypeOld = (select id from ActionPropertyType where name = @ActionPropertyType and actionType_id = @ActionType and deleted = 0 and typeName = 'String');
 set @ActionPropertyTypeNew = (select id from ActionPropertyType where name = @ActionPropertyType and actionType_id = @ActionType and deleted = 0 and typeName = 'Reference');
 
-# UPDATE ActionPropertyType SET idx = 5, penalty = 100, penaltyUserProfile = '29; 7; 19; 32; 31; 53; 36; 37; 57; 48; 63', visibleInDR = 1, userProfile_id = null, userProfileBehaviour = 0 WHERE id = @ActionPropertyTypeNew;
-# UPDATE ActionPropertyType SET idx = 20, penalty = 0, penaltyUserProfile = '7;19', visibleInDR = 0, userProfile_id = 1, userProfileBehaviour = 1 WHERE id = @ActionPropertyTypeOld;
+UPDATE ActionPropertyType SET idx = 5, penalty = 100, penaltyUserProfile = '29; 7; 19; 32; 31; 53; 36; 37; 57; 48; 63', visibleInDR = 1, userProfile_id = null, userProfileBehaviour = 0 WHERE id = @ActionPropertyTypeNew;
+UPDATE ActionPropertyType SET idx = 20, penalty = 0, penaltyUserProfile = '7;19', visibleInDR = 0, userProfile_id = 1, userProfileBehaviour = 1 WHERE id = @ActionPropertyTypeOld;
 
 insert into ActionProperty(createDatetime, createPerson_id, modifyDatetime, modifyPerson_id, deleted, action_id, type_id, unit_id, norm, isAssigned, evaluation, isAutoFillCancelled)
 select *
@@ -147,9 +147,9 @@ where ap.deleted= 0 and ap.type_id = @ActionPropertyTypeOld and apr.id is null g
 # from ActionProperty where type_id = 39729 and deleted = 0);
 
 
-update ActionProperty
-set deleted = 1
-where id in (select id from ActionProperty where type_id = 39729 and deleted = 0);
+# update ActionProperty
+# set deleted = 1
+# where id in (select id from ActionProperty where type_id = 39729 and deleted = 0);
 
 
 select *
