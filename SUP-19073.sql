e698006c2fd119364200f2b205aae5130ecd2d34
Lustik_ak <lustik_alex@nilc.spb.ru>
2021-07-29 17:45:12 +0300

    29-07-2021_17:45

---
+++ SUP-19073.sql
@@ -0,0 +1,9 @@
+Регламент-ХСН-093
+
+
+select *
+from ActionType where group_id = 56182
+
+
+select *
+from ActionType at where at.group_id = 56182 and at.id = (select id from ActionType where group_id = at.group_id order by id desc limit 1)
