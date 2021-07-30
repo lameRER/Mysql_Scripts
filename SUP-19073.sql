4b3082a586b2cece6747f5b0558cc68cd5ccc8b4
Lustik_ak <lustik_alex@nilc.spb.ru>
2021-07-30 13:45:01 +0300

    30-07-2021_13:45

--- SUP-19073.sql
+++ SUP-19073.sql
@@ -49,3 +49,85 @@ from ActionType at where at.group_id = 56182 and at.id = (select id from ActionT
 
 select *
 from ActionType where group_id = 56182 and code regexp @code;
+
+set @num = -1;
+
+select
+       apt.deleted,
+       at.id actionType_id,
+       @num:=@num+1 idx,
+       template_id,
+       apt1.name name,
+       shortName,
+       descr,
+       unit_id,
+       typeName,
+       valueDomain,
+       defaultValue,
+       isVector,
+       norm,
+       apt.sex,
+       apt.age,
+       penalty,
+       penaltyUserProfile,
+       penaltyDiagnosis,
+       visibleInJobTicket,
+       visibleInTableRedactor,
+       isAssignable,
+       test_id,
+       defaultEvaluation,
+       canChangeOnlyOwner,
+       isActionNameSpecifier,
+       laboratoryCalculator,
+       inActionsSelectionTable,
+       redactorSizeFactor,
+       isFrozen,
+       typeEditable,
+       visibleInDR,
+       userProfile_id,
+       userProfileBehaviour,
+       copyModifier,
+       isVitalParam,
+       vitalParamId,
+       isODIIParam,
+       ticketsNeeded,
+       customSelect,
+       autoFieldUserProfile,
+       apt.formulaAlias,
+       incrementOnSave
+from ActionPropertyType apt
+join ActionType at on at.id = 56237
+join (
+    select 'Дата, время поступления вызова' as name
+union
+select 'Дата и время передачи вызова бригаде' as name
+union
+select 'Дата и время выезда на вызов' as name
+union
+select 'Дата, время прибытия на место вызова' as name
+union
+select 'Время появления первых симптомов ОНМК, или время, когда пациента видели здоровым (VIMISSSZ2)' as name
+union
+select 'Уровень сознания, в соответствии со шкалой комы Глазго (VIMISSSZ1)' as name
+union
+select 'Нарушение речи или понимания окружающей обстановки' as name
+union
+select 'АД' as name
+union
+select 'ЧСС' as name
+union
+select 'Сатурация (%)' as name
+union
+select 'Гемипарез (слабость в руке и ноге с одной стороны)' as name
+union
+select 'Форсированный поворот глаз в сторону' as name
+union
+select 'Лечение на догоспитальном этапе (множественный выбор)' as name
+union
+select 'Тип стационара, в который направлен пациент' as name
+union
+select 'Дата и время начала транспортировки пациента' as name
+union
+select 'Дата и время прибытия в МО с пациентом' as name
+    ) as apt1
+where apt.id = (select id from ActionPropertyType where typeName = 'string' order by id desc limit 1);
