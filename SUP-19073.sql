b8aa35da7b28c48dae13dfd47f3a09485a9edddc
Lustik_ak <lustik_alex@nilc.spb.ru>
2021-08-02 11:30:01 +0300

    02-08-2021_11:30

--- SUP-19073.sql
+++ SUP-19073.sql
@@ -134,13 +134,9 @@ where apt.id = (select id from ActionPropertyType where typeName = 'string' orde
 select ActionPropertyType.vitalParamId, ActionPropertyType.isVitalParam, typeName, valueDomain, name, ActionPropertyType.*
 from ActionPropertyType where actionType_id = (select id from ActionType where group_id = @group order by id desc limit 1) and deleted = 0;
 
-select *
-from rbVitalParams v
-join ActionPropertyType apt on apt.vitalParamId = v.id
-where v.id in (296,302,395,394,558,569,454,535,367,356,765,338,314,257,582,287,397,149,141,423,460,313,410,205,501,479,166,445,277,319,156,432,411,541,188,404,347,244,209,612)
 
 select *
-from ActionType where group_id = @group;
+from rbVitalParams where id =395;
 
 select ActionPropertyType.vitalParamId, ActionPropertyType.isVitalParam, typeName, valueDomain, name, ActionPropertyType.*
 from ActionPropertyType where vitalParamId in (
