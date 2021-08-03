e2bbab8a735edd811e365cc96865a615381076b0
Lustik_ak <lustik_alex@nilc.spb.ru>
2021-08-03 12:45:01 +0300

    03-08-2021_12:45

--- GNC-108.sql
+++ GNC-108.sql
@@ -2,7 +2,22 @@ select *
 from Event where externalId = '2021/73034';
 
 
-select *
+select c.*
 from Event e
-join
+left join EventType et on e.eventType_id = et.id
+left join Contract c on c.id = e.contract_id
+left join Contract_PriceList cpl on cpl.contract_id = c.id
+left join PriceList pl on cpl.priceList_id = pl.id
+left join PriceListItem pli on pli.priceList_id = pl.id and pli.serviceCodeOW regexp '12.05.024.001'
+left join rbService s on s.id = pli.service_id
+left join ActionType_Service ats on ats.service_id = s.id
+left join ActionType at on at.id = ats.master_id
+left join rbFinance f on f.id = c.finance_id and f.id = pl.finance_id and et.finance_id = f.id
 where e.id =20439099;
+
+
+select *
+from rbFinance where id =20;
+
+select *
+from PriceListItem where serviceCodeOW regexp '12.05.024.001';
