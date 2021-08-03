25cce608e7d29d0bd49c2c162749c73c3f2d3e71
Lustik_ak <lustik_alex@nilc.spb.ru>
2021-08-03 14:15:01 +0300

    03-08-2021_14:15

--- GNC-108.sql
+++ GNC-108.sql
@@ -17,18 +17,18 @@ where e.id =20439099;
 
 
 
-select pl.*
+select e.id eventID, et.name EventName, c.number Contract, cpl.id ContractPriceList, pl.name PriceList, pli.serviceCodeOW PiceServiceCodeOW , pli.serviceNameOW PriceServiceNameOW, pli.begDate PriceBegDate, pli.endDate PriceEndDate, s.code ServiceCode, s.name ServiceName, s.begDate ServiceBegDate, s.endDate ServiceEndDate, ats.id ActionTypeService, at.code ActionTypeCode, at.name ActionTypeName, f.id finance
 from Event e
- join EventType et on e.eventType_id = et.id
- join Contract c on c.id = e.contract_id
- join Contract_PriceList cpl on cpl.contract_id = c.id
- join PriceList pl on cpl.priceList_id = pl.id
- join PriceListItem pli on pli.priceList_id = pl.id and pli.serviceCodeOW regexp '12.05.024.001'
- join rbService s on s.id = pli.service_id
- join ActionType_Service ats on ats.service_id = s.id
- join ActionType at on at.id = ats.master_id
- join rbFinance f on f.id = c.finance_id and f.id = pl.finance_id and et.finance_id = f.id
-where e.id =20439099;
+ left join EventType et on e.eventType_id = et.id and et.deleted = 0
+ left join Contract c on c.id = e.contract_id and c.deleted=  0
+ left join Contract_PriceList cpl on cpl.contract_id = c.id
+ left join PriceList pl on cpl.priceList_id = pl.id and pl.deleted = 0
+ left join PriceListItem pli on pli.priceList_id = pl.id and pli.deleted = 0 and pli.endDate >= curdate() and pli.serviceCodeOW regexp :code -- '12.05.024.001'
+ left join rbService s on s.id = pli.service_id  and s.endDate >= curdate()
+ left join ActionType_Service ats on ats.service_id = s.id  and ats.endDate >= curdate()
+ left join ActionType at on at.id = ats.master_id and at.deleted =0
+ left join rbFinance f on f.id = c.finance_id and f.id = pl.finance_id and et.finance_id = f.id and f.deleted = 0
+where e.id = :eventId -- 20439099;
 
 
 select *
