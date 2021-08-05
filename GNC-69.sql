a1b7fe2af74d45984fb8a0305d1f5159ad1ebeeb
Lustik_ak <lustik_alex@nilc.spb.ru>
2021-08-05 14:30:01 +0300

    05-08-2021_14:30

--- GNC-69.sql
+++ GNC-69.sql
@@ -727,4 +727,9 @@ where  exists(select * from ActionType where name = tmp.name and code = tmp.code
 
 select *
 from price_gnc_21_08_05 pg
-left join PriceListItem pli on pli.serviceCodeOW = pg.code and (pli.endDate >= curdate() or pli.endDate is null) and pli.priceList_id = 124 group by pli.serviceCodeOW
+left join PriceListItem pli on pli.serviceCodeOW = pg.code and (pli.endDate >= curdate() or pli.endDate is null) and pli.priceList_id = 124
+
+
+
+select *
+from price_gnc_21_08_05 where price is null;
