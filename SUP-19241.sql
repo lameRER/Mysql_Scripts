a5ab81f73abbe7679b03b74cf683187fcc8e8c4c
Lustik_ak <lustik_alex@nilc.spb.ru>
2021-08-05 09:15:01 +0300

    05-08-2021_09:15

--- SUP-19241.sql
+++ SUP-19241.sql
@@ -1,2 +1,5 @@
-select *
-from Person group by userProfile_id;
+# noinspection SqlAggregatesForFile
+
+select up.name, os.name, p.login, p.password
+from Person p, OrgStructure os, rbUserProfile up
+where p.userProfile_id = up.id and os.id = p.orgStructure_id and doctorRoomAccessDenied = 0 group by os.id, up.id order by  up.name, os.name;
