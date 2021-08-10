045ed30bc826125adcd0422a685598667608d0b2
Lustik_ak <lustik_alex@nilc.spb.ru>
2021-08-10 13:15:01 +0300

    10-08-2021_13:15

--- test1234.sql
+++
@@ -1,8 +0,0 @@
-select *
-from OrgStructure_HospitalBed ;
-
-select *
-from EventType where id = 94;
-
-select *
-from Event e where e.eventType_id in(94, 114) and e.setDate >= '2021-08-01' and deleted = 0;
