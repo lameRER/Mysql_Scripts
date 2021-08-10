da23c142667bcce7f2bfdb25a9e247d19850a75b
Lustik_ak <lustik_alex@nilc.spb.ru>
2021-08-10 16:00:01 +0300

    10-08-2021_16:00

--- CHE-398.sql
+++ CHE-398.sql
@@ -6,9 +6,31 @@ from JsonData;
 select ActionPropertyType.customSelect, ActionPropertyType.*
 from ActionPropertyType where actionType_id in
 (select id
-from ActionType where flatCode = 'oper_protocol') and deleted = 0 order by idx, name;
+from ActionType where flatCode = 'oper_protocol') and name = 'Ассистент 2' and deleted = 0 order by idx, name;
 
-%7B%22id%22%3A%2211033%22%2C%22epicriz%22%3A%7B%22fld_0%22%3A%22+%22%2C%22fld_1%22%3A%22I20.8+%5Cu0414%5Cu0440%5Cu0443%5Cu0433%5Cu0438%5Cu0435+%5Cu0444%5Cu043e%5Cu0440%5Cu043c%5Cu044b+%5Cu0441%5Cu0442%5Cu0435%5Cu043d%5Cu043e%5Cu043a%5Cu0430%5Cu0440%5Cu0434%5Cu0438%5Cu0438%22%2C%22fld_2%22%3A%22I01.1+%5Cu041e%5Cu0441%5Cu0442%5Cu0440%5Cu044b%5Cu0439+%5Cu0440%5Cu0435%5Cu0432%5Cu043c%5Cu0430%5Cu0442%5Cu0438%5Cu0447%5Cu0435%5Cu0441%5Cu043a%5Cu0438%5Cu0439+%5Cu044d%5Cu043d%5Cu0434%5Cu043e%5Cu043a%5Cu0430%5Cu0440%5Cu0434%5Cu0438%5Cu0442%22%2C%22fld_3%22%3A%22%22%2C%22fld_4%22%3A%22wefwefew%22%2C%22fld_5%22%3A%22%22%2C%22fld_6%22%3A%22%22%2C%22fld_7%22%3A%22%22%2C%22fld_8%22%3A%22%22%2C%22fld_9%22%3A%22%22%2C%22fld_10%22%3A%22%5Cu043f%5Cu043b%5Cu0430%5Cu043d%5Cu0438%5Cu0440%5Cu0443%5Cu0435%5Cu0442%5Cu0441%5Cu044f%22%7D%7D
+# SQL="
+SELECT apr.value FROM ActionProperty_Person apr
+      WHERE apr.id IN
+            (SELECT ap.id FROM ActionProperty ap
+                WHERE ap.action_id IN
+                  (SELECT a.id FROM Action a
+                  WHERE a.deleted = 0 AND a.id =
+                                          (SELECT parent_id FROM Action a
+                                          WHERE a.id = 104651 ) AND a.actionType_id IN
+                                                                              (SELECT at.id FROM ActionType at
+                                                                              WHERE at.deleted = 0 AND at.serviceType = 4)) AND ap.type_id IN
+                                                                                                                                (SELECT apt.id FROM ActionPropertyType apt
+                                                                                                                                WHERE apt.deleted = 0 AND apt.name = 'Ассистент 2' ));
+# "
+
+
+select *
+from ActionPropertyType where actionType_id = 25596;
+
+select *
+from Action where id = 104648;
+select *
+from ActionProperty where action_id = 104648 and type_id in (SELECT apt.id FROM ActionPropertyType apt WHERE apt.deleted = 0 AND apt.name = 'Ассистент2');
 
 
 select name
