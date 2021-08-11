d85dcdb89af5f7a70b93fa88b57967d50ef4d61f
Lustik_ak <lustik_alex@nilc.spb.ru>
2021-08-11 14:00:03 +0300

    11-08-2021_14:00

--- b15.sql
+++ b15.sql
@@ -1,2 +1,16 @@
+select p.login, p.password, up.name, os.name
+from Person p, OrgStructure os, rbUserProfile up
+where -- p.userProfile_id in (18, 19, 26, 65) and
+#       orgStructure_id = 12
+#   and
+      p.Test_Person = 1
+and os.id = p.orgStructure_id
+and up.id = p.userProfile_id and up.code != ''
+group by p.orgStructure_id, p.userProfile_id order by p.orgStructure_id, p.userProfile_id;
+
+
 select *
-from Person;
+from rbUserProfile where code != '';
+
+
+select * from OrgStructure
