66434f04b622e46a7b0de6d8a63acfbf9ac43cb1
Lustik_ak <lustik_alex@nilc.spb.ru>
2021-08-12 11:30:02 +0300

    12-08-2021_11:30

--- GNC-117.sql
+++ GNC-117.sql
@@ -4,3 +4,8 @@ where name = 'Договор (БЕЗ ПОЛЕЙ, МАСШТАБ - 100%, ДВУС
 or name = 'АКТ (на ТЕКУЩИЙ счёт)'
 or name = 'АКТ (на ВСЕ счета)'
 or name = 'Договор на оказание немедицинской услуги'
+
+
+
+select *
+from rbPrintTemplate where templateText regexp 'elif';
