ec7694d542132525a42ccecdb1d7350b58d8324b
Lustik Alexandr <erpresents@gmail.com>
2021-09-07 11:03:34 +0300

    07-09-2021_11:03

--- HMAO-155.sql
+++ HMAO-155.sql
@@ -112,6 +112,85 @@ from Event where externalId = '484644';
 
 
 
-select *
+select ttj.*
 from TakenTissueJournal ttj
-join Event e on e.client_id = ttj.client_id and e.externalId = ttj.externalId group by ttj.id;
+join Event e on e.client_id = ttj.client_id and e.externalId = ttj.externalId
+where e.id = 247651;
+
+
+
+SELECT  GROUP_CONCAT(CONCAT('GROUP_CONCAT(IF(ttj.datetimeTaken = ', '"', val, '"', ', case when apt.typeName regexp ''String|Text|Constructor'' THEN aps.value when apt.typeName = ''Reference'' THEN apr.value when apt.typeName = ''Date'' THEN apd.value end, NULL)) AS ', '"', valas, '"') SEPARATOR ",
+")  FROM
+         (
+             select DATE_FORMAT(ttj.datetimeTaken, "%d.%m.%y\n%H:%i") valas, ttj.datetimeTaken val,
+                    case
+                        when apt.typeName = 'String' THEN aps.value
+                        when apt.typeName = 'Reference' THEN apr.value
+                        when apt.typeName = 'Date' THEN apd.value end
+             from ActionProperty ap
+                 left join ActionProperty_String aps using(id)
+                 left join ActionProperty_Reference apr using(id)
+                 left join ActionProperty_Date apd using(id)
+                 join Action a on a.id = ap.action_id and a.deleted = 0 and a.event_id = 247651
+                 join Event e2 on e2.id = a.event_id and e2.deleted = 0
+                 join ActionType at2 on at2.id = a.actionType_id and at2.deleted = 0
+                 join ActionType at3 on at3.id = at2.group_id and at3.deleted = 0 and at3.name = 'Лабораторные исследования'
+                 join ActionPropertyType apt on apt.id = ap.type_id and apt.actionType_id = at2.id and at2.id in
+(
+    select DISTINCT apt.actionType_id from ActionPropertyType apt
+    where apt.valueDomain REGEXP '100|107|114|59|61|65|66|67|68|69|73|77|79|80|92|97|98|99') and apt.name != 'Номерок'
+                 join TakenTissueJournal ttj on ttj.id = a.takenTissueJournal_id and ttj.client_id = e2.client_id
+             where ap.deleted = 0 GROUP by ttj.datetimeTaken
+             ) AS top
+
+
+SELECT
+                 apt.Name as `Наименование`, ap.norm as `Норма`
+            	 FROM ActionProperty ap left join ActionProperty_String aps using(id) left join ActionProperty_Reference apr using(id) left join ActionProperty_Date apd using(id) join Action a on a.id = ap.action_id and a.deleted = 0 and a.event_id  = 247651
+            	 join Event e2 on e2.id = a.event_id and e2.deleted = 0 join ActionType at2 on at2.id = a.actionType_id and at2.deleted = 0 join ActionType at3 on (at3.id = at2.group_id and at3.deleted = 0 and at3.name = 'Лабораторные исследования')
+                 join ActionPropertyType apt on apt.id = ap.type_id and apt.actionType_id = at2.id and apt.name != 'Номерок'
+                 join TakenTissueJournal ttj on ttj.id = a.takenTissueJournal_id and ttj.client_id = e2.client_id where ap.deleted = 0 GROUP by at3.id, apt.id
+
+
+select *
+from ActionPropertyType where actionType_id in
+(select id
+from ActionType where group_id in
+(select id
+from ActionType where name = 'Лабораторные исследования'))
+and typeName = 'jobTicket'
+
+
+select *
+from ActionType where group_id in
+(select id
+from ActionType where name = 'Лабораторные исследования')
+
+select *
+from rbEpicrisisProperty;
+
+
+select count(rep.id) num,
+-- GROUP_CONCAT(CONCAT_WS(' ',ret.code, ret.name,'retres.idx:',retres.idx,'resper.idx:',resrep.idx) separator '\n') ret,
+-- GROUP_CONCAT(CONCAT_WS(' ', retres.id, resrep.id, res.id, res.name, res.description)separator '\n') res,
+rep.* from rbEpicrisisTemplates ret
+left join rbEpicrisisTemplates_rbEpicrisisSections retres on retres.id_rbEpicrisisTemplates = ret.id
+left join rbEpicrisisSections res on retres.id_rbEpicrisisSections = res.id
+left join rbEpicrisisSections_rbEpicrisisProperty resrep on resrep.id_rbEpicrisisSections = res.id
+left join rbEpicrisisProperty rep on resrep.id_rbEpicrisisProperty = rep.id
+where ret.id = 132
+group by res.id, rep.id
+order by  retres.idx, resrep.idx;
+
+
+
+select *
+from rbEpicrisisTemplates;
+
+select *
+from rbEpicrisisProperty where id = 2263;
+
+UPDATE s11.rbEpicrisisProperty t
+SET t.defaultValue = 'call procEpicrisisAnalyzes(%s, "''Лабораторные исследования''")'
+WHERE t.id = 2263;
+
