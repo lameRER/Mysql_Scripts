6c4314bad2e007aed6ca6c6aa8c54e1e82f3c20e
Lustik Alexandr <erpresents@gmail.com>
2021-09-13 11:53:32 +0300

    13-09-2021_11:53

--- GNC-134.sql
+++ GNC-134.sql
@@ -1,2 +1,6 @@
 select id, code, name, templateText
 from rbPrintTemplate where templateText regexp 'Заместитель генерального директора.*Троицкая';
+
+
+select *
+from rbPrintTemplate where templateText regexp 'else if';
