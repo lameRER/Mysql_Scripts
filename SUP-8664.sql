2b9cfc4de13bb78be88a46a967223904edc6fa15
Lustik Alexandr <erpresents@gmail.com>
2021-09-20 11:34:58 +0300

    Созданы шаблоны печати

---
+++ SUP-8664.sql
@@ -0,0 +1,29 @@
+insert into rbPrintTemplate(createDatetime, createPerson_id, modifyDatetime, modifyPerson_id, code, name, context, fileName, `default`, dpdAgreement, type, hideParam, banUnkeptDate, counter_id, deleted, isPatientAgreed, groupName, documentType_id, isEditableInWeb, pageOrientation, pageFormat)
+select
+       now() as createDatetime,
+       NULL as createPerson_id,
+       now() as modifyDatetime,
+       NULL as modifyPerson_id,
+       'SUP-8664_2' as code,
+       'Шапка шаблона' as name,
+       'FooterTemplate' as context,
+       fileName,
+       '' as `default`,
+       dpdAgreement,
+       type,
+       hideParam,
+       banUnkeptDate,
+       counter_id,
+       deleted,
+       isPatientAgreed,
+       groupName,
+       documentType_id,
+       isEditableInWeb,
+       pageOrientation,
+       pageFormat
+from rbPrintTemplate order by id desc limit 1;
+
+select * from rbPrintTemplate order by id desc;
+
+select *
+from rbPrintTemplate where id in(3389,3391);
