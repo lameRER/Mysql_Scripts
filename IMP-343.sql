b9f455add63a27a7e29678d7fa3ff16ecc48130d
Lustik Alexandr <erpresents@gmail.com>
2021-09-21 12:30:57 +0300

    Создан тип действия 'Манипуляции'

--- IMP-343.sql
+++ IMP-343.sql
@@ -132,7 +132,7 @@ INSERT INTO ActionPropertyType(deleted, actionType_id, idx, template_id, name, s
 select *
 from
      (select
-             apt.deleted,
+             0 deleted,
              at1.id actionType_id,
              row_number() over ()-1 as idx,
              template_id,
@@ -174,6 +174,159 @@ from
              apt.formulaAlias,
              incrementOnSave
       from ActionPropertyType apt, ActionType at1, temp_apt ta
-      where apt.id = (select id from ActionPropertyType where typeName = 'string' and at1.name = @name and at1.code = @code limit 1)
+      where apt.id = (select id from ActionPropertyType where typeName = 'string' limit 1) and at1.name = @name and at1.code = @code
     ) as tmp
-where not exists(select * from ActionPropertyType where tmp.actionType_id = actionType_id and tmp.name = name);
+where not exists(select * from ActionPropertyType where tmp.actionType_id = actionType_id and tmp.name = name and deleted = tmp.deleted);
+
+select apt.*
+from ActionPropertyType apt
+join ActionType at1 on at1.id = apt.actionType_id and at1.name = @name and at1.code = @code;
+
+
+set @code1 = 'IMP-343';
+set @name1 = 'Манипуляции';
+
+insert into ActionType(createDatetime, createPerson_id, modifyDatetime, modifyPerson_id, deleted, class, group_id, code, name, title, flatCode, sex, age, office, showInForm, genTimetable, quotaType_id, context, amount, amountEvaluation, defaultStatus, defaultDirectionDate, defaultPlannedEndDate, defaultEndDate, defaultExecPerson_id, defaultSetPerson_id, defaultPersonInEvent, defaultPersonInEditor, defaultMKB, defaultMorphology, isMorphologyRequired, defaultOrg_id, showTime, maxOccursInEvent, isMES, nomenclativeService_id, isPreferable, prescribedType_id, shedule_id, isRequiredCoordination, isNomenclatureExpense, hasAssistant, propertyAssignedVisible, propertyUnitVisible, propertyNormVisible, propertyEvaluationVisible, serviceType, actualAppointmentDuration, isSubstituteEndDateToEvent, isPrinted, defaultMES, frequencyCount, frequencyPeriod, frequencyPeriodType, isStrictFrequency, isFrequencyPeriodByCalendar, counter_id, isCustomSum, recommendationExpirePeriod, recommendationControl, isExecRequiredForEventExec, isActiveGroup, lis_code, locked, filledLock, defaultBeginDate, refferalType_id, filterPosts, filterSpecialities, isIgnoreEventExecDate, showAPOrg, showAPNotes, defaultIsUrgent, advancePaymentRequired, checkEnterNote, formulaAlias, isAllowedAfterDeath, isAllowedDateAfterDeath, eventStatusMod)
+select *
+from (
+         select
+                now() as createDatetime,
+                NULL as createPerson_id,
+                now() as modifyDatetime,
+                NULL modifyPerson_id,
+                deleted,
+                class,
+                group_id,
+                @code1 as code,
+                @name1 as name,
+                @name1 as title,
+                flatCode,
+                sex,
+                age,
+                office,
+                showInForm,
+                genTimetable,
+                quotaType_id,
+                context,
+                amount,
+                amountEvaluation,
+                defaultStatus,
+                defaultDirectionDate,
+                defaultPlannedEndDate,
+                defaultEndDate,
+                defaultExecPerson_id,
+                defaultSetPerson_id,
+                defaultPersonInEvent,
+                defaultPersonInEditor,
+                defaultMKB,
+                defaultMorphology,
+                isMorphologyRequired,
+                defaultOrg_id,
+                showTime,
+                maxOccursInEvent,
+                isMES,
+                nomenclativeService_id,
+                isPreferable,
+                prescribedType_id,
+                shedule_id,
+                isRequiredCoordination,
+                isNomenclatureExpense,
+                hasAssistant,
+                propertyAssignedVisible,
+                propertyUnitVisible,
+                propertyNormVisible,
+                propertyEvaluationVisible,
+                serviceType,
+                actualAppointmentDuration,
+                isSubstituteEndDateToEvent,
+                isPrinted,
+                defaultMES,
+                frequencyCount,
+                frequencyPeriod,
+                frequencyPeriodType,
+                isStrictFrequency,
+                isFrequencyPeriodByCalendar,
+                counter_id,
+                isCustomSum,
+                recommendationExpirePeriod,
+                recommendationControl,
+                isExecRequiredForEventExec,
+                isActiveGroup,
+                lis_code,
+                locked,
+                filledLock,
+                defaultBeginDate,
+                refferalType_id,
+                filterPosts,
+                filterSpecialities,
+                isIgnoreEventExecDate,
+                showAPOrg,
+                showAPNotes,
+                defaultIsUrgent,
+                advancePaymentRequired,
+                checkEnterNote,
+                formulaAlias,
+                isAllowedAfterDeath,
+                isAllowedDateAfterDeath,
+                eventStatusMod
+         from ActionType where name = @name and code = @code
+         ) as tmp
+where not exists(select 8 from ActionType where name = tmp.name and code = tmp.code and tmp.deleted = deleted);
+
+drop temporary table if exists temp_apt;
+create temporary table if not exists temp_apt(
+    select * from
+        (select 'Дата' as name union
+         select 'Манипуляция') as tmp
+);
+
+insert into ActionPropertyType(deleted, actionType_id, idx, template_id, name, shortName, descr, unit_id, typeName, valueDomain, defaultValue, isVector, norm, sex, age, penalty, penaltyUserProfile, penaltyDiagnosis, visibleInJobTicket, visibleInTableRedactor, isAssignable, test_id, defaultEvaluation, canChangeOnlyOwner, isActionNameSpecifier, laboratoryCalculator, inActionsSelectionTable, redactorSizeFactor, isFrozen, typeEditable, visibleInDR, userProfile_id, userProfileBehaviour, copyModifier, isVitalParam, vitalParamId, isODIIParam, customSelect, autoFieldUserProfile, formulaAlias, incrementOnSave, ticketsNeeded, parent_id)
+select *
+from
+(select
+       0 deleted,
+       at1.id actionType_id,
+       row_number() over ()-1 idx,
+       template_id,
+       tp.name name,
+       shortName,
+       descr,
+       unit_id,
+       typeName,
+       valueDomain,
+       defaultValue,
+       isVector,
+       norm,
+       apt.sex,
+       apt.age,
+       penalty,
+       penaltyUserProfile,
+       penaltyDiagnosis,
+       visibleInJobTicket,
+       visibleInTableRedactor,
+       isAssignable,
+       test_id,
+       defaultEvaluation,
+       canChangeOnlyOwner,
+       isActionNameSpecifier,
+       laboratoryCalculator,
+       inActionsSelectionTable,
+       redactorSizeFactor,
+       isFrozen,
+       typeEditable,
+       visibleInDR,
+       userProfile_id,
+       userProfileBehaviour,
+       copyModifier,
+       isVitalParam,
+       vitalParamId,
+       isODIIParam,
+       customSelect,
+       autoFieldUserProfile,
+       apt.formulaAlias,
+       incrementOnSave,
+       ticketsNeeded,
+       parent_id
+from ActionPropertyType apt, ActionType at1, temp_apt tp
+where at1.name = @name1 and at1.code = @code1 and apt.id = (select id from ActionPropertyType where typeName = 'String' limit 1)) as tmp
+where not exists(select * from ActionPropertyType where name = tmp.name and actionType_id = tmp.actionType_id and deleted = tmp.deleted);
