029ebf915892c9a8310a2d6826384fa595595443
Lustik Alexandr <erpresents@gmail.com>
2021-09-21 10:24:55 +0300

    Созданы свойства для типа действия 'Протокол сердечно-лёгочной реанимации'

--- SUP-20258.sql
+++ SUP-20258.sql
@@ -119,3 +119,61 @@ from
        eventStatusMod
 from ActionType where group_id is null order by id desc limit 1) as tmp
 where not exists(select * from ActionType where name = tmp.name and code = tmp.code);
+
+drop temporary table if exists temp_apt;
+create temporary table if not exists temp_apt(
+select * from
+    (select 'Манипуляции' as name union
+    select 'Описание') as tmp
+);
+
+
+INSERT INTO ActionPropertyType(deleted, actionType_id, idx, template_id, name, shortName, descr, unit_id, typeName, valueDomain, defaultValue, isVector, norm, sex, age, penalty, penaltyUserProfile, penaltyDiagnosis, visibleInJobTicket, visibleInTableRedactor, isAssignable, test_id, defaultEvaluation, canChangeOnlyOwner, isActionNameSpecifier, laboratoryCalculator, inActionsSelectionTable, redactorSizeFactor, isFrozen, typeEditable, visibleInDR, userProfile_id, userProfileBehaviour, copyModifier, isVitalParam, vitalParamId, isODIIParam, customSelect, autoFieldUserProfile, formulaAlias, incrementOnSave)
+select *
+from
+     (select
+             apt.deleted,
+             at1.id actionType_id,
+             row_number() over ()-1 as idx,
+             template_id,
+             ta.name name,
+             shortName,
+             descr,
+             unit_id,
+             if(ta.name = 'Манипуляции', 'Action', 'String') as typeName,
+             valueDomain,
+             defaultValue,
+             isVector,
+             norm,
+             apt.sex,
+             apt.age,
+             penalty,
+             penaltyUserProfile,
+             penaltyDiagnosis,
+             visibleInJobTicket,
+             visibleInTableRedactor,
+             isAssignable,
+             test_id,
+             defaultEvaluation,
+             canChangeOnlyOwner,
+             isActionNameSpecifier,
+             laboratoryCalculator,
+             inActionsSelectionTable,
+             redactorSizeFactor,
+             isFrozen,
+             typeEditable,
+             visibleInDR,
+             userProfile_id,
+             userProfileBehaviour,
+             copyModifier,
+             isVitalParam,
+             vitalParamId,
+             isODIIParam,
+             customSelect,
+             autoFieldUserProfile,
+             apt.formulaAlias,
+             incrementOnSave
+      from ActionPropertyType apt, ActionType at1, temp_apt ta
+      where apt.id = (select id from ActionPropertyType where typeName = 'string' and at1.name = @name and at1.code = @code limit 1)
+    ) as tmp
+where not exists(select * from ActionPropertyType where tmp.actionType_id = actionType_id and tmp.name = name);
