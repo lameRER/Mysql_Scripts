753387645a9b15033e42752fb6584ad29a5e3f32
Lustik Alexandr <erpresents@gmail.com>
2021-09-30 09:39:12 +0300

    Добавлены дома

--- SUP-20433.sql
+++ SUP-20433.sql
@@ -14,4 +14,26 @@ from STREET where NAME regexp 'матисов') as tmp
 where not exists(select * from STREET where CODE = tmp.CODE);
 
 select *
-from STREET where CODE = '78000000000288200'
+from STREET where CODE = '78000000000288200';
+
+insert into DOMA(NAME, KORP, SOCR, CODE, `INDEX`, GNINMB, UNO, OCATD, flatHouseList)
+select
+       '4литерА,4литерЗ' NAME,
+       KORP,
+       SOCR,
+       '78000000000288200' CODE,
+       '198206' `INDEX`,
+       '7807' GNINMB,
+       UNO,
+       '40279562000' OCATD,
+       '4литерА,4литерЗ' flatHouseList
+from DOMA limit 1;
+
+select *
+from STREET where CODE = '0200000500000070001';
+
+select *
+from KLADR where CODE = '0200000500000070001';
+
+select *
+from DOMA where CODE = '78000000000288200';
