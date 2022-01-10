241e0b4f7e14d58f59c53dd5f890f4617c629ab3
Lustik Alexandr <erpresents@gmail.com>
2022-01-10 16:59:21 +0300

    dev

--- SUP_Petrova-2040.sql
+++ SUP_Petrova-2040.sql
@@ -44,22 +44,53 @@ JOIN rbCureMethod cm1 ON cm1.name = tc.`Метод лечения` AND cm1.isObs
 WHERE pmi.id IS NULL
 GROUP BY tc.`Метод лечения`, tc.`Модель пациента`;
 
-INSERT LOW_PRIORITY rbPatientModel (createDatetime, createPerson_id, modifyDatetime, modifyPerson_id, code, name, MKB, quotaType_id, cureType_id, isObsolete)
-SELECT NOW(),NULL,NOW(),NULL, @return:=@return+1, tc.`Модель пациента`, tc.`Коды по МКБ-10`, (SELECT qt.id FROM QuotaType qt WHERE qt.code REGEXP @quotaType AND qt.code REGEXP tc.`N группы ВМП`), ct1.id, 0 FROM s11.temp_vmp2 tc
-LEFT JOIN rbPatientModel pm ON pm.name = tc.`Модель пациента` AND pm.isObsolete = 0 AND pm.quotaType_id = (SELECT qt.id FROM QuotaType qt WHERE qt.code REGEXP @quotaType AND qt.code REGEXP tc.`N группы ВМП`)
-LEFT JOIN rbPatientModel_Item pmi ON pm.id = pmi.master_id
-LEFT JOIN rbCureType ct ON pm.cureType_id = ct.id AND pmi.cureType_id = ct.id AND ct.isObsolete = 0
-LEFT JOIN rbCureMethod cm ON pmi.cureMethod_id = cm.id AND cm.isObsolete = 0
-LEFT JOIN rbCureType ct1 ON ct1.name = tc.`Вид лечения` AND ct1.isObsolete = 0
-LEFT JOIN rbCureMethod cm1 ON cm1.name = tc.`Метод лечения` AND cm1.isObsolete = 0
-WHERE pm.id IS NULL
-GROUP BY tc.`N группы ВМП`, tc.`Модель пациента`, tc.`Коды по МКБ-10`;
 
-INSERT rbPatientModel_Item (createDatetime, createPerson_id, modifyDatetime, modifyPerson_id, cureType_id, cureMethod_id, master_id)
-SELECT NOW(),NULL,NOW(),NULL, ct1.id, cm1.id, pm.id FROM s11.temp_vmp2 tc
+select *
+from s12.rbCureType where isObsolete = 0;
+
+select *
+from s11.rbCureType where isObsolete = 0;
+
+select *
+from rbCureMethod;
+
+set @return = 2583;
+set @return1 = @return;
+
+insert into rbCureMethod(createDatetime, createPerson_id, modifyDatetime, modifyPerson_id, code, name, regionalCode, isObsolete, EGISZ_code)
+SELECT NOW(),NULL,NOW(),NULL, @return:=@return+1 as code, `Метод лечения` as name, @return1:=@return1+1, 0, null FROM s11.temp_vmp2 tc
 JOIN rbPatientModel pm ON pm.name = tc.`Модель пациента` AND pm.MKB = tc.`Коды по МКБ-10` AND pm.isObsolete = 0 AND pm.quotaType_id IS NOT NULL
 LEFT JOIN rbPatientModel_Item pmi ON pm.id = pmi.master_id
-JOIN rbCureType ct1 ON ct1.name = tc.`Вид лечения` AND ct1.isObsolete = 0 AND ct1.id = pm.cureType_id
-JOIN rbCureMethod cm1 ON cm1.name = tc.`Метод лечения` AND cm1.isObsolete = 0
+left JOIN s11.rbCureType ct1 ON ct1.name = tc.`Вид лечения` AND ct1.isObsolete = 0 AND ct1.id = pm.cureType_id
+left JOIN rbCureMethod cm1 ON cm1.name = tc.`Метод лечения` AND cm1.isObsolete = 0
 WHERE pmi.id IS NULL
 GROUP BY tc.`Метод лечения`, tc.`Модель пациента`;
+
+
+select *
+from s11.rbCureMethod where isObsolete = 0;
+
+
+
+
+select *
+from QuotaType;
+
+
+select *
+from rbPatientModel where quotaType_id = 741;
+
+
+select *
+from rbPatientModel_Item where master_id = 3846;
+
+select *
+from rbCureType where id = 90;
+
+
+select *
+from s11.temp_vmp2 where `N группы ВМП` regexp '09.00.33.001';
+
+
+select *
+from rbCureMethod;
