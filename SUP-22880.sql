9bd502812b69187d23b6884cf2bc802195bd466b
Lustik Alexandr <erpresents@gmail.com>
2022-01-14 10:39:26 +0300

    query

--- SUP-22880.sql
+++ SUP-22880.sql
@@ -6,4 +6,22 @@ from rbprinttemplate where context = '1osm_infekc';
 
 
 select *
-from actionpropertytype where actionType_id = 96751;
+from s11.actionpropertytype where actionType_id = 96751;
+
+
+select apt1.id,
+       concat_ws(' ', apt.deleted, apt1.deleted) as deleted,
+       apt1.name,
+       apt.typeName as OldTypeName,
+       apt1.typeName as NewTypeName
+from actionpropertytype apt
+         join s11.actionpropertytype apt1 on apt.id = apt1.id and (apt.typeName != apt1.typeName or apt.deleted != apt1.deleted)
+where apt1.actionType_id = 96751;
+
+
+select apt.isVitalParam, apt.vitalParamId, apt.*
+from s11.actionpropertytype apt where id = 49881;
+
+
+select *
+from s11.rbSeverityCondition;
