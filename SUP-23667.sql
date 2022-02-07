c3f611bb34f1762e2c2a5217552ff221f37338e5
Lustik Alexandr <erpresents@gmail.com>
2022-02-07 13:51:08 +0300

    dev

--- SUP-23667.sql
+++ SUP-23667.sql
@@ -22,6 +22,10 @@ event e
 
 
 select *
+from eventtype where name regexp '25010 Неотложная помощь детям Стародеревянковская';
+
+
+select *
 from sup_23667;
 
 select *
@@ -65,3 +69,31 @@ from sup_23667 where id = 6875159;
 
 select id, `order`, goal_id
 from event where id = 6932423;
+
+
+
+# create table SUP_23667_2
+# (
+# select *
+# from
+update
+event e
+set e.`order` = 5, goal_id = 11
+where e.eventType_id in (309)
+  and e.setDate between '2022-01-01' and '2022-01-31'
+  and e.deleted = 0
+#   and goal_id = 1
+  and `order` = 0
+# )
+;
+
+
+
+select *
+from event e
+where e.eventType_id in (324, 72, 309)
+  and e.setDate between '2022-01-01' and '2022-01-31'
+  and e.deleted = 0
+  and goal_id = 11
+  and `order` = 5
+;
